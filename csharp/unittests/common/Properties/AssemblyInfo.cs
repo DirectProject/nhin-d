2e13af0d4d4ffb8f434308dee0826c084f99ec69
John Theisen <john.theisen@kryptiq.com>
2010-08-17 22:22:35 -0400

    Removed 'nhin' from the prefix of the commonTests. Altered the build file accordingly.

--- csharp/unittests/common/Properties/AssemblyInfo.cs
+++ csharp/unittests/common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nhinCommonTests")]
+[assembly: AssemblyTitle("commonTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NHIN Direct Project")]
-[assembly: AssemblyProduct("nhinCommonTests")]
+[assembly: AssemblyProduct("commonTests")]
 [assembly: AssemblyCopyright("Copyright © NHIN Direct Project 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
