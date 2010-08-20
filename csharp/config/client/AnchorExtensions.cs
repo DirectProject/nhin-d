22cb6e2ce0bba83642b4ccc8f298e6281b4f6d58
Beau Grantham <bgrantha@harris.com>
2010-08-20 19:51:28 -0400

    Begin cleanup of warnings, code to interfaces

---
+++ csharp/config/client/AnchorExtensions.cs
@@ -0,0 +1,92 @@
+﻿/* 
+ Copyright (c) 2010, NHIN Direct Project
+ All rights reserved.
+
+ Authors:
+    Umesh Madan     umeshma@microsoft.com
+  
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of the The NHIN Direct Project (nhindirect.org). nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using NHINDirect.Config.Store;
+using NHINDirect.Config.Client.CertificateService;
+
+namespace NHINDirect.Config.Client.CertificateService
+{
+    public static class AnchorExtensions
+    {
+        static CertificateGetOptions GetFullCertData = new CertificateGetOptions
+        {
+            IncludeData = true,
+            IncludePrivateKey = true
+        };
+
+        public static Anchor[] GetIncomingAnchors(this AnchorStoreClient client, string owner)
+        {
+            return client.GetIncomingAnchors(owner, GetFullCertData);
+        }
+
+        public static Anchor[] GetOutgoingAnchors(this AnchorStoreClient client, string owner)
+        {
+            return client.GetOutgoingAnchors(owner, GetFullCertData);
+        }
+
+        public static X509Certificate2Collection GetIncomingAnchorX509Certificates(this AnchorStoreClient client, string owner)
+        {
+            return Anchor.ToX509Collection(client.GetIncomingAnchors(owner, GetFullCertData));
+        }
+
+        public static X509Certificate2Collection GetOutgoingAnchorX509Certificates(this AnchorStoreClient client, string owner)
+        {
+            return Anchor.ToX509Collection(client.GetOutgoingAnchors(owner, GetFullCertData));
+        }
+
+        public static IEnumerable<Anchor> EnumerateAnchors(this AnchorStoreClient client, int chunkSize)
+        {
+            return client.EnumerateAnchors(chunkSize, GetFullCertData);
+        }
+
+        public static IEnumerable<Anchor> EnumerateAnchors(this AnchorStoreClient client, int chunkSize, CertificateGetOptions options)
+        {
+            if (chunkSize <= 0 || options == null)
+            {
+                throw new ArgumentException();
+            }
+
+            long lastID = -1;
+
+            Anchor[] anchors;
+            while (true)
+            {
+                anchors = client.EnumerateAnchors(lastID, chunkSize, options);
+                if (anchors == null || anchors.Length == 0)
+                {
+                    yield break;
+                }
+                for (int i = 0; i < anchors.Length; ++i)
+                {
+                    yield return anchors[i];
+                }
+                lastID = anchors[anchors.Length - 1].ID;
+            }
+        }
+
+        public static IEnumerable<X509Certificate2> EnumerateX509Certificates(this AnchorStoreClient client, int chunkSize)
+        {
+            foreach (Anchor cert in client.EnumerateAnchors(chunkSize, GetFullCertData))
+            {
+                yield return cert.ToX509Certificate();
+            }
+        }
+    }
+}
---
+++ csharp/config/store/Anchor.cs
@@ -0,0 +1,204 @@
+﻿/* 
+ Copyright (c) 2010, NHIN Direct Project
+ All rights reserved.
+
+ Authors:
+    Umesh Madan     umeshma@microsoft.com
+  
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of the The NHIN Direct Project (nhindirect.org). nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlTypes;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using NHINDirect;
+using NHINDirect.Certificates;
+
+namespace NHINDirect.Config.Store
+{
+    [Table(Name = "Anchors")]
+    public class Anchor
+    {
+        public const int MaxOwnerLength = 400;
+        
+        string m_owner;
+        byte[] m_data;
+        
+        public Anchor()
+        {
+        }
+
+        public Anchor(string owner, byte[] sourceFileBytes, string password)
+            : this(owner, Certificate.Import(sourceFileBytes, password))
+        {
+        }
+        
+        
+        public Anchor(string owner, X509Certificate2 certificate)
+            : this(owner, certificate, true, true)
+        {
+        }
+        
+        public Anchor(string owner, X509Certificate2 certificate, bool forIncoming, bool forOutgoing)
+        {
+            if (certificate == null)
+            {
+                throw new ConfigStoreException(ConfigStoreError.InvalidCertificate);
+            }
+            
+            this.Owner = owner;
+            this.Thumbprint = certificate.Thumbprint;
+            this.Data = certificate.RawData;
+            this.CreateDate = DateTime.Now;
+            this.ValidStartDate = certificate.NotBefore;
+            this.ValidEndDate = certificate.NotAfter;
+            this.ForIncoming = forIncoming;
+            this.ForOutgoing = forOutgoing;
+        }
+
+        [Column(Name = "Owner", CanBeNull = false, IsPrimaryKey = true)]
+        public string Owner
+        {
+            get
+            {
+                return m_owner;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ConfigStoreException(ConfigStoreError.InvalidOwnerName);
+                }
+                
+                if (value.Length > MaxOwnerLength)
+                {
+                    throw new ConfigStoreException(ConfigStoreError.OwnerLength);
+                }
+                
+                m_owner = value;
+            }
+        }
+
+        [Column(Name = "Thumbprint", CanBeNull = false, IsPrimaryKey = true)]
+        public string Thumbprint
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "CertificateID", CanBeNull = false, IsDbGenerated=true, UpdateCheck = UpdateCheck.Never)]
+        public long ID
+        {
+            get;
+            set;
+        }
+        
+        [Column(Name = "CertificateData", DbType = "varbinary(MAX)", CanBeNull = false, UpdateCheck = UpdateCheck.Never)]
+        public byte[] Data
+        {
+            get
+            {
+                return m_data;
+            }
+            set
+            {
+                m_data = value;
+            }
+        }
+
+        [Column(Name = "CreateDate", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        public DateTime CreateDate
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "ValidStartDate", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        public DateTime ValidStartDate
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "ValidEndDate", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        public DateTime ValidEndDate
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "ForIncoming", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        public bool ForIncoming
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "ForOutgoing", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        public bool ForOutgoing
+        {
+            get;
+            set;
+        }
+
+        public bool HasData
+        {
+            get
+            {
+                return (this.Data != null && this.Data.Length > 0);
+            }
+        }                              
+
+        public bool IsValid(DateTime testDate)
+        {
+            return (this.ValidStartDate <= testDate && testDate <= this.ValidEndDate);
+        }
+
+        public void ClearData()
+        {
+            m_data = null;
+        }
+
+        public void ValidateHasData()
+        {
+            if (m_data.IsNullOrEmpty())
+            {
+                throw new ConfigStoreException(ConfigStoreError.MissingCertificateData);
+            }
+        }
+        
+        public X509Certificate2 ToX509Certificate()
+        {
+            return new X509Certificate2(this.Data);
+        }
+        
+        public static X509Certificate2Collection ToX509Collection(Anchor[] anchors)
+        {
+            if (anchors.IsNullOrEmpty())
+            {
+                return null;
+            }
+            
+            X509Certificate2Collection x509Collection = new X509Certificate2Collection();
+            if (anchors != null)
+            {
+                for (int i = 0; i < anchors.Length; ++i)
+                {
+                    x509Collection.Add(anchors[0].ToX509Certificate());
+                }
+            }            
+            return x509Collection;
+        }
+    }
+}
