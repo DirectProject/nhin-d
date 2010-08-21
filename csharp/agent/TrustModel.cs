6b43300c313bf58ae3846f5277e1afc28b48e355
Arien Malec <arien.malec@nhindirect.org>
2010-08-20 22:01:50 -0700

    style cleanup of TrustModel

--- csharp/agent/TrustModel.cs
+++ csharp/agent/TrustModel.cs
@@ -4,6 +4,7 @@
 
  Authors:
     Umesh Madan     umeshma@microsoft.com
+    Arien Malec     arien.malec@nhindirect.org
   
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
