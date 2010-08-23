9294ceb4c2279df09adfd1060f65a3916e75bcf9
Beau Grantham <bgrantha@harris.com>
2010-08-23 16:29:25 -0400

    Adding commons-lang dependency

---
+++ csharp/unittests/agent/DnsResolverTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+
+using NHINDirect.Certificates;
+
+using Xunit;
+using Xunit.Extensions;
+
+namespace AgentTests
+{
+    public class DnsResolverTest
+    {
+        public const string ServerIP = "127.0.0.1";
+        
+        public static IEnumerable<object[]> GoodAddresses
+        {
+			get
+			{
+				yield return new[] {"bob@nhind.hsgincubator.com"};
+				yield return new[] {"biff@nhind.hsgincubator.com"};
+				yield return new[] {"gatewaytest@hotmail.com"};
+			}
+        }
+        
+        [Theory(Skip = "Requires Bind Server to be running on the local server")]
+		[PropertyData("GoodAddresses")]
+        public void GetCertificateWithGoodAddress(string address)
+        {
+        	var resolver
+        		= new DnsCertResolver(IPAddress.Parse(ServerIP), 5000, "hsgincubator.com")
+        		  	{
+        		  		AssumeWildcardSupport = false
+        		  	};
+
+        	X509Certificate2Collection certs = resolver.GetCertificates(new MailAddress(address));
+        	Assert.True(certs.Count > 0);
+        }
+    }
+}
