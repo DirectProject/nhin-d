634e962298f1d0789e3fff27e98dc067d3d63c61
umeshma <umeshma@microsoft.com>
2010-08-23 12:56:01 -0700

    1. Updated Config Schema 2. Added timeout value to settings

--- csharp/config/store/Schema.sql
+++ csharp/config/store/Schema.sql
@@ -1,14 +1,21 @@
 ﻿USE [NHINDConfig]
 GO
-/****** Object:  Table [dbo].[Domains]    Script Date: 08/18/2010 07:04:15 ******/
+/****** Object:  Table [dbo].[Accounts]    Script Date: 08/23/2010 12:34:41 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-SET ANSI_PADDING ON
-GOUSE [NHINDConfig]
+CREATE TABLE [dbo].[Accounts](
+	[AccountID] [bigint] IDENTITY(1,1) NOT NULL,
+	[AccountName] [nvarchar](255) NOT NULL,
+	[CreateDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED 
+(
+	[AccountID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Domains]    Script Date: 08/18/2010 07:04:15 ******/
+/****** Object:  Table [dbo].[Domains]    Script Date: 08/23/2010 12:34:41 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -35,7 +42,7 @@ CREATE UNIQUE NONCLUSTERED INDEX [IX_Domains_DomainID] ON [dbo].[Domains]
 	[DomainID] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Certificates]    Script Date: 08/18/2010 07:04:15 ******/
+/****** Object:  Table [dbo].[Certificates]    Script Date: 08/23/2010 12:34:41 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -65,7 +72,7 @@ CREATE UNIQUE NONCLUSTERED INDEX [IX_Certificates_CertificateID] ON [dbo].[Certi
 	[CertificateID] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Anchors]    Script Date: 08/18/2010 07:04:15 ******/
+/****** Object:  Table [dbo].[Anchors]    Script Date: 08/23/2010 12:34:41 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -91,7 +98,7 @@ CREATE TABLE [dbo].[Anchors](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  Table [dbo].[Addresses]    Script Date: 08/18/2010 07:04:15 ******/
+/****** Object:  Table [dbo].[Addresses]    Script Date: 08/23/2010 12:34:41 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -120,22 +127,25 @@ CREATE UNIQUE NONCLUSTERED INDEX [IX_Addresses_AddressID] ON [dbo].[Addresses]
 	[AddressID] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 GO
-/****** Object:  Default [DF_Addresses_Status]    Script Date: 08/18/2010 07:04:15 ******/
-ALTER TABLE [dbo].[Addresses] ADD  CONSTRAINT [DF_Addresses_Status]  DEFAULT ((0)) FOR [Status]
+/****** Object:  Default [DF_Accounts_CreateDate]    Script Date: 08/23/2010 12:34:41 ******/
+ALTER TABLE [dbo].[Accounts] ADD  CONSTRAINT [DF_Accounts_CreateDate]  DEFAULT (getdate()) FOR [CreateDate]
 GO
-/****** Object:  Default [DF_Anchors_ForIncoming]    Script Date: 08/18/2010 07:04:15 ******/
-ALTER TABLE [dbo].[Anchors] ADD  CONSTRAINT [DF_Anchors_ForIncoming]  DEFAULT ((1)) FOR [ForIncoming]
-GO
-/****** Object:  Default [DF_Anchors_ForOutgoing]    Script Date: 08/18/2010 07:04:15 ******/
-ALTER TABLE [dbo].[Anchors] ADD  CONSTRAINT [DF_Anchors_ForOutgoing]  DEFAULT ((1)) FOR [ForOutgoing]
-GO
-/****** Object:  Default [DF_Certificates_CreateDate]    Script Date: 08/18/2010 07:04:15 ******/
+/****** Object:  Default [DF_Certificates_CreateDate]    Script Date: 08/23/2010 12:34:41 ******/
 ALTER TABLE [dbo].[Certificates] ADD  CONSTRAINT [DF_Certificates_CreateDate]  DEFAULT (getdate()) FOR [CreateDate]
 GO
-/****** Object:  Default [DF_Certificates_Status]    Script Date: 08/18/2010 07:04:15 ******/
+/****** Object:  Default [DF_Certificates_Status]    Script Date: 08/23/2010 12:34:41 ******/
 ALTER TABLE [dbo].[Certificates] ADD  CONSTRAINT [DF_Certificates_Status]  DEFAULT ((0)) FOR [Status]
 GO
-/****** Object:  ForeignKey [FK_Addresses_DomainID]    Script Date: 08/18/2010 07:04:15 ******/
+/****** Object:  Default [DF_Anchors_ForIncoming]    Script Date: 08/23/2010 12:34:41 ******/
+ALTER TABLE [dbo].[Anchors] ADD  CONSTRAINT [DF_Anchors_ForIncoming]  DEFAULT ((1)) FOR [ForIncoming]
+GO
+/****** Object:  Default [DF_Anchors_ForOutgoing]    Script Date: 08/23/2010 12:34:41 ******/
+ALTER TABLE [dbo].[Anchors] ADD  CONSTRAINT [DF_Anchors_ForOutgoing]  DEFAULT ((1)) FOR [ForOutgoing]
+GO
+/****** Object:  Default [DF_Addresses_Status]    Script Date: 08/23/2010 12:34:41 ******/
+ALTER TABLE [dbo].[Addresses] ADD  CONSTRAINT [DF_Addresses_Status]  DEFAULT ((0)) FOR [Status]
+GO
+/****** Object:  ForeignKey [FK_Addresses_DomainID]    Script Date: 08/23/2010 12:34:41 ******/
 ALTER TABLE [dbo].[Addresses]  WITH CHECK ADD  CONSTRAINT [FK_Addresses_DomainID] FOREIGN KEY([DomainID])
 REFERENCES [dbo].[Domains] ([DomainID])
 GO
