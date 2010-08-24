2e26d6c5f60c9b12ce05839919829fb40497526d
umeshma <umeshma@microsoft.com>
2010-08-23 19:45:13 -0700

    Fixed BuildGateway.sln build problem

---
+++ csharp/common/Cryptography/SignedEntity.cs
@@ -0,0 +1,136 @@
+﻿/* 
+ Copyright (c) 2010, NHIN Direct Project
+ All rights reserved.
+
+ Authors:
+    Umesh Madan     umeshma@microsoft.com
+  
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of the The NHIN Direct Project (nhindirect.org). nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+
+using NHINDirect.Mime;
+
+namespace NHINDirect.Cryptography
+{
+    public class SignedEntity : MultipartEntity
+    {
+        MimeEntity m_content;
+        MimeEntity m_signature;
+        
+        public SignedEntity(DigestAlgorithm algorithm, MimeEntity content, MimeEntity signature)
+            : base(CreateContentType(algorithm))
+        {
+            if (content == null)
+            {
+                throw new ArgumentNullException();
+            }
+            
+            Content = content;
+            Signature = signature;
+        }
+        
+        public SignedEntity(ContentType contentType, IEnumerable<MimeEntity> parts)
+            : base(contentType)
+        {
+            if (parts == null)
+            {
+                throw new ArgumentNullException();
+            }
+            
+            int count = 0;
+            foreach(MimeEntity part in parts)
+            {
+                switch(count)
+                {
+                    default:
+                        throw new SignatureException(SignatureError.InvalidSignedEntity);
+                    
+                    case 0:
+                        Content = part;
+                        break;
+                    
+                    case 1:
+                        Signature = part;
+                        break;
+                }                
+                ++count;
+            }
+        }
+        
+        public MimeEntity Content
+        {
+            get
+            {
+                return m_content;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                
+                m_content = value;
+            }
+        }
+        
+        public MimeEntity Signature
+        {
+            get
+            {
+                return m_signature;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                
+                if (!SMIMEStandard.IsDetachedSignature(value))
+                {
+                    throw new SignatureException(SignatureError.NotDetachedSignature);
+                }
+                m_signature = value;
+            }
+        }
+                
+        public override IEnumerator<MimeEntity> GetEnumerator()
+        {
+            yield return m_content;
+            yield return m_signature;
+        }
+        
+        public static SignedEntity Load(MimeEntity source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.IsMultiPart)
+            {
+                throw new SignatureException(SignatureError.InvalidMultipartSigned);
+            }
+            
+            return new SignedEntity(source.ParsedContentType, source.GetParts());
+        }
+        
+        static ContentType CreateContentType(DigestAlgorithm digestAlgorithm)
+        {
+            ContentType contentType = new ContentType(SMIMEStandard.MultiPartTypeSigned);
+            contentType.Parameters.Add(SMIMEStandard.ProtocolParameterKey, SMIMEStandard.SignatureProtocol);
+			contentType.Parameters.Add(SMIMEStandard.MICAlgorithmKey, digestAlgorithm.AsString());
+            return contentType;
+        }
+    }
+}
---
+++ csharp/unittests/agent/AddressTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+
+using Xunit;
+using Xunit.Extensions;
+
+using NHINDirect.Agent;
+
+namespace AgentTests
+{
+    public class AddressTest
+    {
+
+        [Fact]
+        public void TestBasicAddressCreate()
+        {
+            NHINDAddress addr = new NHINDAddress("eleanor@roosevelt.com");
+            Assert.Null(addr.Certificates);
+            Assert.False(addr.HasCertificates);
+            Assert.Null(addr.TrustAnchors);
+            Assert.False(addr.HasTrustAnchors);
+            Assert.Equal<TrustEnforcementStatus>(TrustEnforcementStatus.Unknown, addr.Status);
+            Assert.False(addr.IsTrusted(TrustEnforcementStatus.Success));
+            Assert.Equal<string>("eleanor", addr.User);
+            Assert.Equal<string>("roosevelt.com", addr.Host);
+        }
+
+        [Fact]
+        public void TestAddressTrustStatus()
+        {
+            NHINDAddress addr = new NHINDAddress(new MailAddress("\"Eleanor Roosevelt\" <eleanor@roosevelt.org>"));
+            addr.Certificates = new X509Certificate2Collection(new X509Certificate2());
+            addr.TrustAnchors = new X509Certificate2Collection(new X509Certificate2());
+            Assert.True(addr.HasCertificates);
+            Assert.True(addr.HasTrustAnchors);
+            addr.Status = TrustEnforcementStatus.Success;
+            Assert.True(addr.IsTrusted(TrustEnforcementStatus.Success));
+            addr.Status = TrustEnforcementStatus.Failed;
+            Assert.False(addr.IsTrusted(TrustEnforcementStatus.Success));
+        }
+
+        [Fact]
+        public void TestBasicAdddressCollectionCreate()
+        {
+            NHINDAddressCollection coll = new NHINDAddressCollection();
+            Assert.False(coll.IsTrusted());
+            Assert.Equal<string>("", coll.ToString());
+            Assert.False(coll.IsTrusted(TrustEnforcementStatus.Failed));
+            Assert.True(coll.GetCertificates().Count == 0);
+            Assert.True(coll.Certificates.Count() == 0);
+            Assert.True(coll.GetUntrusted().Count() == 0);
+            Assert.True(coll.GetTrusted().Count() == 0);
+            Assert.DoesNotThrow(() => coll.RemoveUntrusted());
+        }
+
+        [Fact]
+        public void TestAddressCollectionTrust()
+        {
+            NHINDAddressCollection coll = BasicCollection();
+            Assert.False(coll.IsTrusted());
+            Assert.True(coll.IsTrusted(TrustEnforcementStatus.Unknown));
+            foreach (NHINDAddress addr in coll)
+            {
+                addr.Status = TrustEnforcementStatus.Success;
+            }
+            Assert.True(coll.IsTrusted());
+        }
+
+        [Theory]
+        [InlineData(null,0)]
+        [InlineData("", 0)]
+        [InlineData("eleanor@roosevelt.com, \"Franklin Roosevelt\" <frank@roosevelt.com>, sean+o'nolan@tinymollitude.net", 3)]
+        public void TestParseAddressCollection(string addressList, int expectedCount)
+        {
+            NHINDAddressCollection coll = NHINDAddressCollection.Parse(addressList);
+            Assert.Equal<int>(expectedCount, coll.Count);
+        }
+
+        [Fact]
+        public void TestAddressCollectionTrustedUntrusted()
+        {
+            NHINDAddressCollection coll = BasicCollection();
+            coll[0].Status = TrustEnforcementStatus.Failed;
+            coll[1].Status = TrustEnforcementStatus.Unknown;
+            coll[2].Status = TrustEnforcementStatus.Success;
+
+            Assert.False(coll.IsTrusted());
+            Assert.Equal(1, coll.GetTrusted().Count());
+            Assert.Equal(2, coll.GetUntrusted().Count());
+            Assert.Equal(2, coll.GetTrusted(TrustEnforcementStatus.Unknown).Count());
+
+            coll.RemoveUntrusted();
+            Assert.Equal(1, coll.Count);
+        }
+
+        NHINDAddressCollection BasicCollection()
+        {
+            string[] addrStrings = new string[] {
+                "eleanor@roosevelt.com",
+                "\"Franklin Roosevelt\" <frank@roosevelt.com>",
+                "sean+o'nolan@tinymollitude.net"};
+            IEnumerable<NHINDAddress> addrs =  addrStrings.Select(a => new NHINDAddress(a));
+            NHINDAddressCollection coll = new NHINDAddressCollection();
+            coll.Add(addrs);
+            return coll;
+        }
+
+    }
+}
