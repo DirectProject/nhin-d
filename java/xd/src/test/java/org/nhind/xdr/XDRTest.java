56feea147f5817329f2457c083d2d822518f7fc1
Beau Grantham <bgrantha@harris.com>
2010-08-24 09:46:52 -0400

    Additional XMLUtils refactoring

--- java/xd/src/test/java/org/nhind/xdr/XDRTest.java
+++ java/xd/src/test/java/org/nhind/xdr/XDRTest.java
@@ -5,18 +5,16 @@
 package org.nhind.xdr;
 
 import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.Marshaller;
-import javax.xml.namespace.QName;
 
-import org.nhind.util.XMLUtils;
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
 
 import junit.framework.TestCase;
 import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
+
+import org.nhind.util.XMLUtils;
 
 /**
  *
@@ -75,41 +73,17 @@ public class XDRTest extends TestCase {
         String sresult = null;
 
         try {
-
             qname = new QName("urn:oasis:names:tc:ebxml-regrep:xsd:rs:3.0", "RegistryResponseType");
 
-            sresult = marshalResponse(qname, result);
+            sresult = XMLUtils.marshal(qname, result, oasis.names.tc.ebxml_regrep.xsd.rs._3.ObjectFactory.class);
         } catch (Exception x) {
             x.printStackTrace();
             fail("Failed unmarshalling response");
         }
-        // System.out.println(expResponse);
-        //  System.out.println(sresult);
+
+        // System.out.println(sresult);
         assertTrue(sresult.indexOf("ResponseStatusType:Success") >= 0);
 
-    }
-
-
-
-    protected String marshalResponse(QName altName, Object jaxb) {
-
-        String ret = null;
-        try {
-
-            javax.xml.bind.JAXBContext jc = javax.xml.bind.JAXBContext.newInstance(oasis.names.tc.ebxml_regrep.xsd.rs._3.ObjectFactory.class);
-            Marshaller u = jc.createMarshaller();
-
-            StringWriter sw = new StringWriter();
-            u.marshal(new JAXBElement(altName, jaxb.getClass(), jaxb), sw);
-            StringBuffer sb = sw.getBuffer();
-            ret = new String(sb);
-
-        } catch (Exception ex) {
-            Logger.getLogger(this.getClass().getPackage().getName()).log(Level.INFO, "marshall. Exception msg=" + ex.getMessage());
-            ex.printStackTrace();
-
-        }
-        return ret;
     }
 
     /**
