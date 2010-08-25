9fb1f2964049d76aa9cea81660dc56d48351aab8
Arien Malec <arien.malec@nhindirect.org>
2010-08-24 18:59:59 -0700

    Documentation fix to DnsCert and documentation and tests for StringSegment

---
+++ csharp/config/store/EntityStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHINDirect.Config.Store
+{
+    public enum EntityStatus : byte
+    {
+        New = 0,
+        Enabled = 1,
+        Disabled = 2,
+        Deleted = 3
+    }
+}
