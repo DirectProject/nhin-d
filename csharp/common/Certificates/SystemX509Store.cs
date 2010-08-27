35db72bcac1b1c9c42ccd3221d3d6bbaa089f63f
umeshma <umeshma@microsoft.com>
2010-08-27 15:21:37 -0700

    Automated merge with https://nhin-d.googlecode.com/hg/

--- csharp/common/Certificates/SystemX509Store.cs
+++ csharp/common/Certificates/SystemX509Store.cs
@@ -23,10 +23,18 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace NHINDirect.Certificates
 {
+    /// <summary>
+    /// Represents a local machine store for certificates.
+    /// </summary>
     public class SystemX509Store : CertificateStore
     {
         X509Store m_store;
         
+        /// <summary>
+        /// Initializes an instance from an existing store with a criteria on for accepted certificates.
+        /// </summary>
+        /// <param name="store">The store to draw certificates from</param>
+        /// <param name="criteria">The predicate to test accepted certificates.</param>
         public SystemX509Store(X509Store store, Predicate<X509Certificate2> criteria)
             : base(criteria)
         {
@@ -38,6 +46,11 @@ namespace NHINDirect.Certificates
             m_store = store;
         }
                 
+        /// <summary>
+        /// Indexes certificates by the subject name attribute.
+        /// </summary>
+        /// <param name="subjectName">The subject name for which to look up certificates.</param>
+        /// <returns>The certificates for the subject name.</returns>
         public override X509Certificate2Collection this[string subjectName]
         {
             get
@@ -46,11 +59,20 @@ namespace NHINDirect.Certificates
             }
         }
                 
+        /// <summary>
+        /// Indicates if the certificate store contains the specified <paramref name="cert"/>
+        /// </summary>
+        /// <param name="cert">The certificate to search for.</param>
+        /// <returns><c>true</c> if the store contains the certificate, <c>false</c> if not.</returns>
         public override bool Contains(X509Certificate2 cert)
         {
             return m_store.Certificates.Contains(cert);
         }
         
+        /// <summary>
+        /// Adds the <paramref name="cert"/> to this store.
+        /// </summary>
+        /// <param name="cert">The certificate to add. Must match the criteria for this store.</param>
         public override void Add(X509Certificate2 cert)
         {
             if (cert == null)
@@ -65,6 +87,10 @@ namespace NHINDirect.Certificates
             }
         }
 
+        /// <summary>
+        /// Removes a certificate from this store.
+        /// </summary>
+        /// <param name="cert">The certificate to remove.</param>
         public override void Remove(X509Certificate2 cert)
         {
             lock(m_store)
@@ -73,6 +99,10 @@ namespace NHINDirect.Certificates
             }
         }
                         
+        /// <summary>
+        /// Returns the enumeration of all the certificates matching the store criteria in this store.
+        /// </summary>
+        /// <returns>The enumeration of store certificates.</returns>
         public override IEnumerator<X509Certificate2> GetEnumerator()
         {
             X509Certificate2Collection certs;
@@ -84,6 +114,9 @@ namespace NHINDirect.Certificates
             return certs.Enumerate(this.Criteria).GetEnumerator();
         }
                 
+        /// <summary>
+        /// Frees resources for this instance.
+        /// </summary>
         public override void Dispose()
         {
             if (m_store != null)
@@ -96,8 +129,17 @@ namespace NHINDirect.Certificates
         /*
             Names of the default NHIND Machine Certificate Stores
         */        
+        /// <summary>
+        /// The default machine store for anchor certificates
+        /// </summary>
         public const string AnchorCertsStoreName = "NHINDAnchors";
+        /// <summary>
+        /// The default machine store for private certificates
+        /// </summary>
         public const string PrivateCertsStoreName = "NHINDPrivate";
+        /// <summary>
+        /// The default maching store for external public certificates.
+        /// </summary>
         public const string ExternalCertsStoreName = "NHINDExternal";
         /*
             These methods work with the default NHIND Certificate Stores
@@ -120,28 +162,56 @@ namespace NHINDirect.Certificates
             store.Dispose();
         }
         
+        /// <summary>
+        /// Opens the default anchor machine certificate store for reads.
+        /// </summary>
+        /// <returns>The default anchor machine certificate store.</returns>
         public static SystemX509Store OpenAnchor()
         {
             return new SystemX509Store(Extensions.OpenStoreRead(AnchorCertsStoreName, StoreLocation.LocalMachine), null);
         }
+        /// <summary>
+        /// Opens the default anchor machine certificate store for reads and writes.
+        /// </summary>
+        /// <returns>The default anchor machine certificate store.</returns>
         public static SystemX509Store OpenAnchorEdit()
         {
             return new SystemX509Store(Extensions.OpenStoreReadWrite(AnchorCertsStoreName, StoreLocation.LocalMachine), null);
         }
+
+        /// <summary>
+        /// Opens the default private certificate store for reads
+        /// </summary>
+        /// <returns>The default private certificate store.</returns>
         public static SystemX509Store OpenPrivate()
         {
             return new SystemX509Store(Extensions.OpenStoreRead(PrivateCertsStoreName, StoreLocation.LocalMachine),
                                         x => x.HasPrivateKey);
         }
+
+        /// <summary>
+        /// Opens the default private certificate store for reads and writes
+        /// </summary>
+        /// <returns>The default private certificate store.</returns>
         public static SystemX509Store OpenPrivateEdit()
         {
             return new SystemX509Store(Extensions.OpenStoreReadWrite(PrivateCertsStoreName, StoreLocation.LocalMachine),
                                         x => x.HasPrivateKey);
         }
+
+        /// <summary>
+        /// Opens the default external machine certificate store for reads.
+        /// </summary>
+        /// <returns>The default external certificate store.</returns>
         public static SystemX509Store OpenExternal()
         {
             return new SystemX509Store(Extensions.OpenStoreRead(ExternalCertsStoreName, StoreLocation.LocalMachine), null);
         }
+
+        /// <summary>
+        /// Opens the default external machine certificate store for reads and writes.
+        /// </summary>
+        /// <returns>The default external certificate store.</returns>
         public static SystemX509Store OpenExternalEdit()
         {
             return new SystemX509Store(Extensions.OpenStoreReadWrite(ExternalCertsStoreName, StoreLocation.LocalMachine), null);
