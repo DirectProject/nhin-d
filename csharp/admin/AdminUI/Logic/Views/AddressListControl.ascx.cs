8ac87070ac14ddb8dc28ca29832ed5bb0b7a778b
Arien Malec <arien.malec@nhindirect.org>
2010-08-26 22:56:30 -0700

    docu for UniformCertificateStore

---
+++ csharp/admin/AdminUI/Logic/Views/AddressListControl.ascx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using NHINDirect.Config.Client.DomainManager;
+using NHINDirect.Config.Store;
+
+namespace AdminUI.Logic.Views
+{
+    public partial class AddressListControl : System.Web.UI.UserControl
+    {
+        private NHINDirect.Config.Client.DomainManager.AddressManagerClient _addressManagerClient =
+            new AddressManagerClient();
+
+        private IEnumerable<Address> _model;
+        public const int MAXRESULTSPERPAGE = 20;
+        public event EventHandler<AddressListControlEventArgs> Command;
+        public string Owner
+        {
+            get
+            {
+                if (ViewState["Owner"] != null)
+                {
+                    return ViewState["Owner"] as string;
+                }
+                else
+                {
+                    return String.Empty;
+                }
+            }
+            set { ViewState["Owner"] = value; }
+        }
+        public long DomainId
+        {
+            get
+            {
+                long returnValue = -1;
+                if (ViewState["DomainId"] != null)
+                {
+
+                    long.TryParse(ViewState["DomainId"].ToString(), out returnValue);
+
+                }
+                return returnValue;
+            }
+            set { ViewState["DomainId"] = value; }
+        }
+
+    
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UpdateModel();
+            Command += delegate(object o, AddressListControlEventArgs ea) { };
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            //DataBindControls();
+            AddressesGridView.DataSource = _model;
+            AddressesGridView.DataBind();
+        }
+        private void UpdateModel()
+        {
+            bool filterByOwner = false;
+
+            if (!string.IsNullOrEmpty(Request.QueryString["Owner"]))
+            {
+                filterByOwner = true;
+                Owner = Request.QueryString["Owner"];
+
+                // TODO: Do not rely on DomainID for filtering. Change it once they service API is updated
+                long domainId = -1;
+                long.TryParse(Request.QueryString["DomainId"], out domainId);
+                DomainId = domainId;
+                OwnerTitleContainer.Visible = true;
+                OwnerTitleLabel.Text = Owner;
+
+            }
+
+            _model = filterByOwner ? _addressManagerClient.EnumerateDomainAddresses(DomainId, MAXRESULTSPERPAGE)
+                : _addressManagerClient.EnumerateAddresses(MAXRESULTSPERPAGE);
+        }
+
+        protected void AddressesGridView_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            long addressId = WebHelper.GetDataKeyFromGridView(sender, e.CommandArgument, "ID");
+            string emailAddress = WebHelper.GetDataKeyAsObjectFromGridView( sender, e.CommandArgument, "EmailAddress").ToString();
+            long domainId = WebHelper.GetDataKeyFromGridView(sender, e.CommandArgument, "DomainID");
+
+            //TODO: Provide Owner 
+            Command(this, new AddressListControlEventArgs(addressId,emailAddress , domainId,  e.CommandName));
+
+        }
+        public class AddressListControlEventArgs : EventArgs
+        {
+            public long AddressId { get; private set; }
+            public long DomainId { get; private set; }
+            public string CommandName { get; private set; }
+            public string EmailAddress { get; private set; }
+
+            public AddressListControlEventArgs(long addressId, string emailAddress, long domainId, string commandName)
+            {
+                AddressId = addressId;
+                EmailAddress = emailAddress;
+                CommandName = commandName;
+                DomainId = domainId;
+            }
+
+        }
+    }
+}
---
+++ csharp/config/store/Certificate.cs
@@ -0,0 +1,233 @@
+﻿/* 
+ Copyright (c) 2010, NHIN Direct Project
+ All rights reserved.
+
+ Authors:
+    Umesh Madan     umeshma@microsoft.com
+  
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of the The NHIN Direct Project (nhindirect.org). nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlTypes;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using NHINDirect.Certificates;
+using System.Runtime.Serialization;
+
+namespace NHINDirect.Config.Store
+{    
+    [Table(Name="Certificates")]
+    [DataContract(Namespace = ConfigStore.Namespace)]
+    public class Certificate
+    {
+        public const int MaxOwnerLength = 400;
+        
+        string m_owner;
+        byte[] m_data;
+                
+        public Certificate()
+        {
+        }
+        
+        public Certificate(string owner, byte[] sourceFileBytes, string password)
+            : this(owner, Import(sourceFileBytes, password))
+        {
+        }
+        
+        public Certificate(string owner, X509Certificate2 certificate)
+        {
+            if (certificate == null)
+            {
+                throw new ConfigStoreException(ConfigStoreError.InvalidX509Certificate);
+            }
+            
+            this.Owner = owner;
+            this.SetX509Certificate(certificate);
+        }
+
+        [Column(Name = "Owner", CanBeNull = false, IsPrimaryKey = true)]
+        [DataMember(IsRequired=true)]
+        public string Owner
+        {
+            get
+            {
+                return m_owner;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ConfigStoreException(ConfigStoreError.InvalidOwnerName);
+                }
+
+                if (value.Length > MaxOwnerLength)
+                {
+                    throw new ConfigStoreException(ConfigStoreError.OwnerLength);
+                }
+                
+                m_owner = value;
+            }
+        }
+
+        [Column(Name = "Thumbprint", CanBeNull = false, IsPrimaryKey = true)]
+        [DataMember(IsRequired = true)]
+        public string Thumbprint
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "CertificateID", CanBeNull = false, IsDbGenerated=true, UpdateCheck = UpdateCheck.Never)]
+        [DataMember(IsRequired = true)]
+        public long ID
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "CreateDate", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        [DataMember(IsRequired = true)]
+        public DateTime CreateDate
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "CertificateData", DbType = "varbinary(MAX)", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        [DataMember(IsRequired = true)]
+        public byte[] Data
+        {
+            get
+            {
+                return m_data;
+            }
+            set
+            {                
+                m_data = value;
+            }
+        }
+
+        [Column(Name = "ValidStartDate", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        [DataMember(IsRequired = true)]
+        public DateTime ValidStartDate
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "ValidEndDate", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        [DataMember(IsRequired = true)]
+        public DateTime ValidEndDate
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "Status", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        [DataMember(IsRequired = true)]
+        public EntityStatus Status
+        {
+            get;
+            set;
+        }
+        
+        public bool HasData
+        {
+            get
+            {
+                return (this.Data != null && this.Data.Length > 0);
+            }
+        }                              
+        
+        public bool IsValid(DateTime testDate)
+        {
+            return (this.ValidStartDate <= testDate && testDate <= this.ValidEndDate);
+        }
+        
+        public void ClearData()
+        {
+            m_data = null;
+        }
+        
+        public void ValidateHasData()
+        {
+            if (m_data.IsNullOrEmpty())
+            {
+                throw new ConfigStoreException(ConfigStoreError.MissingCertificateData);
+            }        
+        }
+        
+        public void ExcludePrivateKey()
+        {
+            if (this.HasData)
+            {
+                this.SetX509Certificate(this.ToPublicX509Certificate());
+            }
+        }
+        
+        public X509Certificate2 ToX509Certificate()
+        {
+            return new X509Certificate2(this.Data);
+        }
+
+        public X509Certificate2 ToPublicX509Certificate()
+        {
+            X509Certificate2 cert = this.ToX509Certificate();
+            if (cert.HasPrivateKey)
+            {
+                byte[] certBytes = cert.Export(X509ContentType.Cert);
+                cert = new X509Certificate2(certBytes);
+            }            
+            
+            return cert;
+        }
+
+        public static X509Certificate2Collection ToX509Collection(Certificate[] source)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                return null;
+            }
+            
+            X509Certificate2Collection x509Coll = new X509Certificate2Collection();
+            if (source != null)
+            {
+                for (int i = 0; i < source.Length; ++i)
+                {
+                    x509Coll.Add(source[i].ToX509Certificate());
+                }
+            }
+            return x509Coll;
+        }
+                
+        void SetX509Certificate(X509Certificate2 certificate)
+        {
+            this.Thumbprint = certificate.Thumbprint;
+            this.Data = certificate.Export(X509ContentType.Pfx, string.Empty);
+            this.CreateDate = DateTime.Now;
+            this.ValidStartDate = certificate.NotBefore;
+            this.ValidEndDate = certificate.NotAfter;
+        }
+        
+        internal static X509Certificate2 Import(byte[] sourceFileBytes, string password)
+        {
+            if (sourceFileBytes.IsNullOrEmpty())
+            {
+                throw new ConfigStoreException(ConfigStoreError.InvalidX509Certificate);
+            }
+
+            return new X509Certificate2(sourceFileBytes, password ?? string.Empty, X509KeyStorageFlags.Exportable);
+        }
+    }
+}
