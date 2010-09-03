7ffdf6e5ca472027ce70d9072a80d7022cee4f03
vincent lewis <vincent.lewis@gsihealth.com>
2010-09-03 15:44:55 -0400

    xdmail

---
+++ java/xdmail/src/main/java/ihe/iti/xds_b/_2007/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * IHE XDS.b Document Repository
+ * 
+ */
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:ihe:iti:xds-b:2007", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package ihe.iti.xds_b._2007;
