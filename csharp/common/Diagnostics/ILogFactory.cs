95764d5b28284138dea343c953a30de84c2d6e46
John Theisen <john.theisen@kryptiq.com>
2010-09-07 17:45:29 -0700

    Moved the logging interface into nhinCommon

--- csharp/common/Diagnostics/ILogFactory.cs
+++ csharp/common/Diagnostics/ILogFactory.cs
@@ -13,7 +13,9 @@ Neither the name of the The NHIN Direct Project (nhindirect.org). nor the names 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  
 */
-namespace Health.Net.Diagnostics
+using System;
+
+namespace NHINDirect.Diagnostics
 {
 	/// <summary>
 	/// Defines a factory class that creates an instance of an logging framework specific logger.
@@ -21,10 +23,17 @@ namespace Health.Net.Diagnostics
 	public interface ILogFactory
 	{
 		/// <summary>
-		/// Give the <paramref name="name"/> used as a key, return a specific <see cref="ILogger"/> instance.
+		/// Given the <paramref name="name"/> used as a key, return a specific <see cref="ILogger"/> instance.
 		/// </summary>
 		/// <param name="name">The name is used as the key to obtain an instance of the <see cref="ILogger"/></param>
 		/// <returns>An instance of <see cref="ILogger"/></returns>
 		ILogger GetLogger(string name);
+
+		///<summary>
+		/// Given the <paramref name="loggerType"/>'s FullName used as a key, return a specific <see cref="ILogger"/> instance.
+		///</summary>
+		///<param name="loggerType"></param>
+		///<returns></returns>
+		ILogger GetLogger(Type loggerType);
 	}
 }
