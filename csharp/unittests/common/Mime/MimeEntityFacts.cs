bade97c6b42b5b92171088e362e2b19e70ca4977
Arien Malec <arien.malec@nhindirect.org>
2010-09-07 17:19:18 -0700

    Unit tests and bugfix for MimeEntity

---
+++ csharp/unittests/common/Mime/MimeEntityFacts.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NHINDirect.Mime;
+using Xunit;
+
+namespace NHINDirect.Tests.Mime
+{
+    public class MimeEntityFacts
+    {
+
+
+        [Fact]
+        public void DefaultConstructorShouldCreateEmptyEntity()
+        {
+            MimeEntity e = new MimeEntity();
+            Assert.Empty(e.Headers);
+            Assert.False(e.HasBody);
+            Assert.False(e.HasHeaders);
+            Assert.False(e.IsMultiPart);
+        }
+
+        [Fact]
+        public void TextConstructiorShouldCreateTextPlain()
+        {
+            MimeEntity e = new MimeEntity("Hello, world");
+            Assert.True(e.HasMediaType("text/plain"));
+        }
+
+        [Fact]
+        public void BodyTextShouldBeAccessible()
+        {
+            MimeEntity e = new MimeEntity("Hello, world");
+            Assert.Equal("Hello, world", e.Body.Text);
+        }
+
+        [Fact]
+        public void BodySourceTextShouldBeAccessible()
+        {
+            MimeEntity e = new MimeEntity("Hello, world");
+            Assert.Equal("Hello, world", e.Body.SourceText.ToString());
+        }
+    }
+}
