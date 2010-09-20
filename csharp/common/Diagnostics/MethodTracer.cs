2294f5ebb02aa6a0d2b309f7e07002fea9ab5388
John Theisen <john.theisen@kryptiq.com>
2010-09-20 10:17:39 -0700

    Added a simple method (code block) tracer that logs at the Trace level the begin (on construction) and end on Dispose. This should be used in conjuction with a 'using' statement to auto-dispose the object.

---
+++ csharp/common/Diagnostics/MethodTracer.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Diagnostics;
+
+namespace NHINDirect.Diagnostics
+{
+    ///<summary>
+    /// A convience class that allows the implementor to surround a block of code
+    /// and automatically trace the begin and end of the code (even when an exception
+    /// occurs).
+    ///</summary>
+    /// <example>
+    /// // here is an example of usage
+    /// using (new MethodTracer("methodName", this))
+    /// {
+    ///     // your method's code here
+    /// }
+    /// </example>
+    public class MethodTracer : IDisposable
+    {
+        private const string MethodBeginSuffix = "_Begin";
+        private const string MethodEndSuffix = "_End";
+
+        private readonly ILogger m_logger;
+        private readonly string m_methodName;
+
+        ///<summary>
+        /// Trace the begin and end of a block of code. 
+        ///</summary>
+        ///<param name="methodName">The method name or prefix of the string to use.</param>
+        ///<param name="logOwner">The owner object to obtain the logger for</param>
+        public MethodTracer(string methodName, object logOwner)
+            : this(methodName, Log.For(logOwner))
+        {
+        }
+
+        ///<summary>
+        /// Trace the begin and end of a block of code using the <paramref name="logger"/>
+        ///</summary>
+        ///<param name="methodName">The method name or prefix of the string to use.</param>
+        ///<param name="logger">The logger to log to at the Trace level</param>
+        public MethodTracer(string methodName, ILogger logger)
+        {
+            m_logger = logger;
+            m_methodName = methodName;
+            m_logger.Trace(methodName + MethodBeginSuffix);
+        }
+
+        ///<summary>
+        /// Trace the begin and end of a block of code using the <paramref name="logger"/>.
+        /// The method name will be obtained by grabbing the name of the method one
+        /// frame up the StackFrame.
+        ///</summary>
+        ///<param name="logger">The logger to log to at the Trace level</param>
+        public MethodTracer(ILogger logger)
+        {
+            m_logger = logger;
+            if (m_logger.IsTraceEnabled)
+            {
+                m_methodName = new StackTrace().GetFrame(1).GetMethod().Name;
+            }
+
+            m_logger.Trace(m_methodName + MethodBeginSuffix);
+        }
+
+        public void Dispose()
+        {
+            if (m_logger.IsTraceEnabled && !string.IsNullOrEmpty(m_methodName))
+            {
+                m_logger.Trace(m_methodName + MethodEndSuffix);
+            }
+        }
+    }
+}
