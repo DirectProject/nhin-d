4234496b43d68d6652043643eb3168df55ef60c1
Mark Bamberg <bambergm@gmail.comg>
2010-09-28 11:55:41 -0400

    Automatic install

---
+++ csharp/admin/AdminUI/Global.asax.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Web;
+
+using Health.Net.Diagnostics.NLog;
+
+using NHINDirect.Container;
+using NHINDirect.Diagnostics;
+
+namespace AdminUI
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            IoC.Initialize(new SimpleDependencyResolver())
+                .Register<ILogFactory>(
+                new NLogFactory(
+                    new LogFileSettings
+                        {
+                            DirectoryPath = HttpContext.Current.Server.MapPath(@"~\Log"),
+                            Level = LoggingLevel.Debug,
+                            EventLogLevel = LoggingLevel.Fatal,
+                            EventLogSource = "nhin AdminUI",
+                            Ext = ".log",
+                            FileChangeFrequency = 24,
+                            NamePrefix = "adminui"
+                        }
+                    )
+                )
+                ;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
