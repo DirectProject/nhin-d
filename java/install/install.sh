47ef5be7f804633452caec57e711046e3dd78ed2
Mark Bamberg <bambergm@gmail.com>
2010-09-29 00:52:42 -0400

    Correct relative path problem in Unix/Linux build

--- java/install/install.sh
+++ java/install/install.sh
@@ -1,18 +1,20 @@
 #!/bin/sh -x
 if [ $# -gt 0 ] ; then
-	export DIRECT_INSTALL_DIR=$1
+	export DIRECT_INSTALL_DIR_REL=$1
 else
-	export DIRECT_INSTALL_DIR=build
+	export DIRECT_INSTALL_DIR_REL=build
 fi
-if [ ! -d $DIRECT_INSTALL_DIR ]; then
-	echo "Installation directory $DIRECT_INSTALL_DIR does not exist"
+if [ ! -d $DIRECT_INSTALL_DIR_REL ]; then
+	echo "Installation directory $DIRECT_INSTALL_DIR_REL does not exist"
 	exit 1
 fi
-if [ -d $DIRECT_INSTALL_DIR/james-2.3.2 -o -e $DIRECT_INSTALL_DIR/james-2.3.2 ]; then
-	echo "Can't install to $DIRECT_INSTALL_DIR - file or directory exists"
+export DIRECT_INSTALL_DIR=`cd $DIRECT_INSTALL_DIR_REL; pwd`
+export DIRECT_INSTALL_PACKAGE_DIR=${DIRECT_INSTALL_DIR}/james-2.3.2
+if [ -d $DIRECT_INSTALL_PACKAGE_DIR -o -e $DIRECT_INSTALL_PACKAGE_DIR ]; then
+	echo "Can't install to $DIRECT_INSTALL_PACKAGE_DIR - file or directory exists"
 	exit 1
 else
-	echo "Installing Direct in $1"
+	echo "Installing Direct in $DIRECT_INSTALL_PACKAGE_DIR"
 	export DIRECT_INSTALL_CD=`pwd`
 	ant -f src/scripts/build.xml
 fi
