af4557d5a609ffeafd7ac2c428e07888f6e1b408
Mark Bamberg <bambergm@gmail.com>
2010-09-29 13:36:08 -0400

    Mac OS X Install

--- java/install/install.sh
+++ java/install/install.sh
@@ -3,6 +3,14 @@ if [ $# -gt 0 ] ; then
 	export DIRECT_INSTALL_DIR_REL=$1
 else
 	export DIRECT_INSTALL_DIR_REL=build
+	if [ ! -d $DIRECT_INSTALL_DIR_REL -a ! -e $DIRECT_INSTALL_DIR_REL ]; then
+		mkdir $DIRECT_INSTALL_DIR_REL
+	else
+		if [ ! -d $DIRECT_INSTALL_DIR_REL ]; then
+			echo "Can't make directory $DIRECT_INSTALL_DIR_REL"
+			exit 1
+		fi
+	fi
 fi
 if [ ! -d $DIRECT_INSTALL_DIR_REL ]; then
 	echo "Installation directory $DIRECT_INSTALL_DIR_REL does not exist"
@@ -16,5 +24,5 @@ if [ -d $DIRECT_INSTALL_PACKAGE_DIR -o -e $DIRECT_INSTALL_PACKAGE_DIR ]; then
 else
 	echo "Installing Direct in $DIRECT_INSTALL_PACKAGE_DIR"
 	export DIRECT_INSTALL_CD=`pwd`
-	ant -f src/scripts/build.xml
+	ant -v -f src/scripts/build.xml
 fi
