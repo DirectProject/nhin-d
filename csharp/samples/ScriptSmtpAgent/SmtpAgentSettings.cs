abfa8b5b0534fd14323b0bb7b1f8aeccda07030f
Beau Grantham <bgrantha@harris.com>
2010-10-03 18:23:46 -0400

    Unit tests

---
+++ csharp/samples/ScriptSmtpAgent/SmtpAgentSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHINDirect.Agent.Config;
+using NHINDirect.Diagnostics;
+using System.Xml.Serialization;
+
+namespace NHINDirect.ScriptAgent
+{
+    [XmlType("SmtpAgentConfig")]
+    public class SmtpAgentSettings : AgentSettings
+    {
+        public SmtpAgentSettings()
+        {
+        }
+        
+        [XmlElement("Log")]
+        public LogFileSettings LogSettings
+        {
+            get;
+            set;
+        }
+        
+        public override void Validate()
+        {
+            if (this.LogSettings == null)
+            {
+                throw new ArgumentNullException("Log Settings not specified");
+            }
+            this.LogSettings.Validate();
+        }
+        
+        public static SmtpAgentSettings LoadFile(string configFilePath)
+        {
+            return AgentSettings.LoadFile<SmtpAgentSettings>(configFilePath);
+        }
+    }
+}
