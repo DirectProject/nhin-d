6231fd17041fec45569c92185eda82426e84524f
Beau Grantham <bgrantha@harris.com>
2010-10-06 13:30:06 -0400

    XD metadata abstraction (consume)

--- java/client/src/test/java/org/nhindirect/nhindclient/NHINDClientTest.java
+++ java/client/src/test/java/org/nhindirect/nhindclient/NHINDClientTest.java
@@ -94,7 +94,7 @@ public class NHINDClientTest extends TestCase
 //
 //        NHINDClientImpl instance = new NHINDClientImpl("gmail-smtp.l.google.com");
 //        String messageId = UUID.randomUUID().toString();
-//        String endpoint = "http://ELS4055:8080/xd/services/DocumentRepository_Service";
+//        String endpoint = "http://localhost:8080/xd/services/DocumentRepository_Service";
 //        String result = instance.send(endpoint, meta, docs, messageId);
 //        String expResult = "urn:oasis:names:tc:ebxml-regrep:ResponseStatusType:Success";
 //        assertEquals(expResult, result);
@@ -119,9 +119,9 @@ public class NHINDClientTest extends TestCase
     public void testSend() throws Exception
     {
         String sender = "lewistower1@gmail.com";
-//        Collection<String> receivers = Arrays.asList("beau+receiver@id84.com", "beau+receiver2@id84.com",
-//            "http://ELS4055:8080/xd/services/DocumentRepository_Service");
-        Collection<String> receivers = Arrays.asList("beau+receiver@nologs.org", "beau+receiver2@nologs.org");     
+        Collection<String> receivers = Arrays.asList("beau+receiver@nologs.org", "beau+receiver2@nologs.org",
+            "http://ELS4055:8080/xd/services/DocumentRepository_Service");
+//        Collection<String> receivers = Arrays.asList("beau+receiver@nologs.org", "beau+receiver2@nologs.org");     
 
         DirectMessage message = new DirectMessage(sender, receivers);
 
