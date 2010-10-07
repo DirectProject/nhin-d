6e721b631eb40427cb87a09dc96f6ff125143996
John Theisen <john.theisen@kryptiq.com>
2010-10-07 15:48:12 -0700

    Modified to reduce the total amount logged to the console by default, optionally more verbose logging can be requested.

--- csharp/msb.bat
+++ csharp/msb.bat
@@ -1,6 +1,9 @@
-echo off
+@echo off
 setlocal
 set default_buildfile=build.xml
+set msbuild_verbosity=/v:minimal
+
+if "%1" EQU "help" goto :help
 
 rem this is here if we want to support different names of the build file in the future
 if exist %default_buildfile% (
@@ -10,16 +13,32 @@ if exist %default_buildfile% (
 if not exist %buildfile% goto :error_missing_buildfile
 
 for %%i in (%*) do call :append_arg %%i
-msbuild %buildfile%%options%
+msbuild %msbuild_verbosity% %buildfile%%options%
 goto :done
 
 :append_arg
-set options=%options% -t:%1
+if "%1" EQU "verbose" (
+	set msbuild_verbosity=/v:normal
+) else (
+  if "%1" NEQ "help" set options=%options% -t:%1
+)
 goto :eof
 
 :error_missing_buildfile
 echo Missing build file - %buildfile%!
 goto :eof
+
+:help
+echo usage: msb [target] ... [targetN]
+echo            execute one or more targets found in %buildfile%
+echo.
+echo        msb verbose [target] ... [targetN]
+echo            same as above with more verbose logging
+echo.
+echo        msb help
+echo            this message
+echo.
+exit /B
 
 :done
 endlocal
