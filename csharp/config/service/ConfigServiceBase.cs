cf8f523bacbe2352befbac8617cef9053176e112
John Theisen <john.theisen@kryptiq.com>
2010-10-07 21:41:05 -0700

    Pulled the common logging and 'config store' access into a common base class.

---
+++ csharp/config/service/ConfigServiceBase.cs
@@ -0,0 +1,36 @@
+using System;
+using System.ServiceModel;
+
+using NHINDirect.Config.Store;
+using NHINDirect.Diagnostics;
+
+namespace NHINDirect.Config.Service
+{
+    public class ConfigServiceBase
+    {
+        private readonly ConfigStore m_store;
+        private readonly ILogger m_logger;
+
+        public ConfigServiceBase()
+        {
+            m_store = Service.Current.Store;
+            m_logger = Log.For(this);
+        }
+
+        private ILogger Logger
+        {
+            get { return m_logger; }
+        }
+
+        protected ConfigStore Store
+        {
+            get { return m_store; }
+        }
+
+        protected FaultException<ConfigStoreFault> CreateFault(string methodName, Exception ex)
+        {
+            Logger.Error(string.Format("While performing {0}()", methodName), ex);
+            return Service.CreateFault(ex);
+        }
+    }
+}
