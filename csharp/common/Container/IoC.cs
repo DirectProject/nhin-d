0a5359782b5b39c37bebb8736993b3879fcedfb1
John Theisen <john.theisen@kryptiq.com>
2010-10-26 16:01:59 -0700

    Revisions made to allow the configuration of the 'container' to be specified in the app.config or web.config file. This has not been implemented in any of the applications yet, issues to be debugged!

--- csharp/common/Container/IoC.cs
+++ csharp/common/Container/IoC.cs
@@ -3,7 +3,7 @@
  All rights reserved.
 
  Authors:
-    John Theisen     jtheisen@kryptiq.com
+    John Theisen
   
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
