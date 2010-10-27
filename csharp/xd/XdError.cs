a5535555df85a15af65ee3601865719fb932d8f9
Vassil Peytchev <vassil@epic.com>
2010-10-27 17:02:05 -0500

    Namespace is case sensitive (Xds vs XDS)

---
+++ csharp/xd/XdError.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Health.Direct.Xd
+{
+    /// <summary>
+    /// Error codes for generating or consuming XD* Metadata
+    /// </summary>
+    public enum XdError
+    {
+        /// <summary>
+        /// An entity (Association, DocumentEntry, SubmissionSet) is missing the id attribute
+        /// </summary>
+        MissingId,
+    }
+}
