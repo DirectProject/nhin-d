abf9eb6633e005a577a913dedb7b904ad108f3bd
Beau Grantham <bgrantha@harris.com>
2010-11-01 15:04:24 -0400

    Fix typo in setdomain.sh

--- java/install/src/scripts/setdomain.sh
+++ java/install/src/scripts/setdomain.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-sed -i "s/<servername>localhost<\/servername>/<servername>postmaster@$1<\/servername>/g" $DIRECT_HOME/james-2.3.2/apps/james/SAR-INF/config.xml
+sed -i "s/<servername>localhost<\/servername>/<servername>$1<\/servername>/g" $DIRECT_HOME/james-2.3.2/apps/james/SAR-INF/config.xml
 sed -i "s/<postmaster>Postmaster@localhost<\/postmaster>/<postmaster>postmaster@$1<\/postmaster>/g" $DIRECT_HOME/james-2.3.2/apps/james/SAR-INF/config.xml
 
 echo "Domain set to $1"
