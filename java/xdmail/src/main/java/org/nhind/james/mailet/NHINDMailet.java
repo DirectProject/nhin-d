68f2924bb7ecaf991f51443e1cb1e2c487b14151
Beau Grantham <bgrantha@harris.com>
2010-11-03 15:41:14 -0400

    Checkin 1 of xd/agent integration

--- java/xdmail/src/main/java/org/nhind/james/mailet/NHINDMailet.java
+++ java/xdmail/src/main/java/org/nhind/james/mailet/NHINDMailet.java
@@ -30,16 +30,24 @@ package org.nhind.james.mailet;
 
 import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import javax.mail.Address;
 import javax.mail.MessagingException;
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.AddressException;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.mailet.Mail;
+import org.apache.mailet.MailAddress;
 import org.apache.mailet.base.GenericMailet;
 import org.nhind.mail.service.DocumentRepository;
+import org.nhindirect.xd.routing.RoutingResolver;
+import org.nhindirect.xd.routing.impl.RoutingResolverImpl;
 import org.nhindirect.xd.transform.MimeXdsTransformer;
 import org.nhindirect.xd.transform.impl.DefaultMimeXdsTransformer;
 
@@ -49,48 +57,101 @@ import org.nhindirect.xd.transform.impl.DefaultMimeXdsTransformer;
  * XDR Document Recipient via IHE XDS.b Provide and Register transaction
  * (ITI-41).
  */
-public class NHINDMailet extends GenericMailet {
-    
+public class NHINDMailet extends GenericMailet
+{
+
     private String endpointUrl;
-    
+    private String configServiceUrl;
+
     private MimeXdsTransformer mimeXDSTransformer;
     private DocumentRepository documentRepository;
+    private RoutingResolver resolver;
 
     private static final Log LOGGER = LogFactory.getFactory().getInstance(NHINDMailet.class);
-    
+
     /*
      * (non-Javadoc)
      * 
      * @see org.apache.mailet.base.GenericMailet#service(org.apache.mailet.Mail)
      */
     @Override
-    public void service(Mail mail) throws MessagingException {
+    public void service(Mail mail) throws MessagingException
+    {
         LOGGER.info("Servicing NHINDMailet");
 
-        if (StringUtils.isBlank(endpointUrl)) {
+        if (StringUtils.isBlank(endpointUrl))
+        {
             LOGGER.error("NHINDMailet endpoint URL cannot be empty or null.");
             throw new MessagingException("NHINDMailet endpoint URL cannot be empty or null.");
         }
 
-        try
+        // Get recipients and create a collection of Strings
+        List<String> recipAddresses = new ArrayList<String>();
+
+        for (Object addr : mail.getRecipients())
         {
-            List<ProvideAndRegisterDocumentSetRequestType> requests = getMimeXDSTransformer().transform(mail.getMessage());
+            recipAddresses.add(((MailAddress) addr).toString());
+        }
 
-            for (ProvideAndRegisterDocumentSetRequestType request : requests)
+        // Service XD* addresses
+        if (getResolver().hasXdEndpoints(recipAddresses))
+        {
+            LOGGER.info("Recipients include XD endpoints");
+            
+            try
             {
-                String response = getDocumentRepository().forwardRequest(endpointUrl, request);
+                // Extract XD* addresses
+                List<Address> xdAddresses = new ArrayList<Address>();
+                for (String s : getResolver().getXdEndpoints(recipAddresses))
+                    xdAddresses.add((new MailAddress(s)).toInternetAddress());
 
-                if (!isSuccessful(response))
-                    throw new MessagingException("Unsuccessful response received");
+                // Replace recipients with only XD* addresses
+                mail.getMessage().setRecipients(RecipientType.TO, xdAddresses.toArray(new Address[0]));
+
+                List<ProvideAndRegisterDocumentSetRequestType> requests = getMimeXDSTransformer().transform(
+                        mail.getMessage());
+
+                for (ProvideAndRegisterDocumentSetRequestType request : requests)
+                {
+                    String response = getDocumentRepository().forwardRequest(endpointUrl, request);
+
+                    if (!isSuccessful(response))
+                    {
+                        LOGGER.error("NHINDMailet failed to deliver XD message.");
+                        LOGGER.error(response);
+                    }
+                }
             }
+            catch (Throwable e)
+            {
+                LOGGER.error("NHINDMailet delivery failure", e);
+            }
+        }
 
+        // Service SMTP addresses (fall through)
+        if (getResolver().hasSmtpEndpoints(recipAddresses))
+        {
+            LOGGER.info("Recipients include SMTP endpoints");
+            
+            mail.setRecipients(getSmtpRecips(recipAddresses));
+        }
+        else
+        {
+            LOGGER.info("Recipients do not include SMTP endpoints");
+            
+            // No SMTP addresses, ghost it
             mail.setState(Mail.GHOST);
         }
-        catch (Throwable e)
-        {
-            LOGGER.error("NHINDMailet delivery failure", e);
-            throw new RuntimeException(e);
-        }
+    }
+
+    private Collection<MailAddress> getSmtpRecips(Collection<String> recips) throws AddressException
+    {
+        List<MailAddress> addrs = new ArrayList<MailAddress>();
+
+        for (String s : getResolver().getSmtpEndpoints(recips))
+            addrs.add(new MailAddress(s));
+
+        return addrs;
     }
 
     private boolean isSuccessful(String response)
@@ -107,45 +168,106 @@ public class NHINDMailet extends GenericMailet {
      * @see org.apache.mailet.base.GenericMailet#init()
      */
     @Override
-    public void init() throws MessagingException {
+    public void init() throws MessagingException
+    {
         LOGGER.info("Initializing NHINDMailet");
-        
-        // Get the configuration URL
+
+        // Get the endpoint URL
         endpointUrl = getInitParameter("EndpointURL");
-        
+
         if (StringUtils.isBlank(endpointUrl))
         {
             LOGGER.error("NHINDMailet endpoint URL cannot be empty or null.");
             throw new MessagingException("NHINDMailet endpoint URL cannot be empty or null.");
-        }   
+        }
+
+        // Get the config-service URL
+        try
+        {
+            configServiceUrl = getInitParameter("ConfigURL");
+        }
+        catch (Exception e)
+        {
+            // eat it
+        }
     }
-    
+
     /**
      * Return the value of endpointUrl.
      * 
      * @return the value of endpointUrl.
      */
-    protected String getEndpointUrl() {
+    protected String getEndpointUrl()
+    {
         return this.endpointUrl;
     }
-    
+
     /**
      * Set the value of endpointUrl.
      * 
      * @param endpointUrl
      *            The value of endpointUrl.
      */
-    protected void setEndpointUrl(String endpointUrl) {
+    protected void setEndpointUrl(String endpointUrl)
+    {
         this.endpointUrl = endpointUrl;
     }
-    
+
+    /**
+     * Return the value of configServiceUrl.
+     * 
+     * @return the value of configServiceUrl.
+     */
+    protected String getConfigServiceUrl()
+    {
+        return this.configServiceUrl;
+    }
+
+    /**
+     * Set the value of configServiceUrl.
+     * 
+     * @param configServiceUrl
+     *            The value of configServiceUrl.
+     */
+    protected void setConfigServiceUrl(String configServiceUrl)
+    {
+        this.configServiceUrl = configServiceUrl;
+    }
+
+    /**
+     * Set the value of resolver.
+     * 
+     * @param resolver
+     *            The value of resolver.
+     */
+    protected void setResolver(RoutingResolver resolver)
+    {
+        this.resolver = resolver;
+    }
+
+    /**
+     * Get the value of resolver.
+     * 
+     * @return the value of resolver.
+     */
+    protected RoutingResolver getResolver()
+    {
+        if (this.resolver == null)
+        {
+            this.resolver = new RoutingResolverImpl(configServiceUrl);
+        }
+
+        return resolver;
+    }
+
     /**
      * Set the value of mimeXDSTransformer.
      * 
      * @param mimeXDSTransformer
      *            The value of mimeXDSTransformer.
      */
-    protected void setMimeXDSTransformer(MimeXdsTransformer mimeXDSTransformer) {
+    protected void setMimeXDSTransformer(MimeXdsTransformer mimeXDSTransformer)
+    {
         this.mimeXDSTransformer = mimeXDSTransformer;
     }
 
@@ -154,11 +276,13 @@ public class NHINDMailet extends GenericMailet {
      * 
      * @return the value of mimeXDSTransformer, or a new object if null.
      */
-    protected MimeXdsTransformer getMimeXDSTransformer() {
-        if (this.mimeXDSTransformer == null) {
+    protected MimeXdsTransformer getMimeXDSTransformer()
+    {
+        if (this.mimeXDSTransformer == null)
+        {
             this.mimeXDSTransformer = new DefaultMimeXdsTransformer();
         }
-        
+
         return this.mimeXDSTransformer;
     }
 
@@ -168,7 +292,8 @@ public class NHINDMailet extends GenericMailet {
      * @param documentRepository
      *            The value of documentRepository.
      */
-    public void setDocumentRepository(DocumentRepository documentRepository) {
+    public void setDocumentRepository(DocumentRepository documentRepository)
+    {
         this.documentRepository = documentRepository;
     }
 
@@ -177,11 +302,13 @@ public class NHINDMailet extends GenericMailet {
      * 
      * @return the value of documentRepository, or a new object if null.
      */
-    public DocumentRepository getDocumentRepository() {
-        if (this.documentRepository == null) {
+    public DocumentRepository getDocumentRepository()
+    {
+        if (this.documentRepository == null)
+        {
             this.documentRepository = new DocumentRepository();
         }
-        
+
         return documentRepository;
     }
 }
