7a04900f61d2141ee6204182bce824bff3a95628
John Theisen <john.theisen@kryptiq.com>
2010-11-05 16:10:31 -0700

    The new MVC version of the Admin UI (not finalized - more modifications to come)

---
+++ csharp/admin/AdminMvc/Controllers/DomainsController.cs
@@ -0,0 +1,86 @@
+﻿using System.Web.Mvc;
+
+using AdminMvc.Models;
+
+using Health.Direct.Config.Store;
+
+namespace AdminMvc.Controllers
+{
+    public class DomainsController : ControllerBase<Domain, DomainRepository>
+    {
+        public ActionResult Addresses(long id)
+        {
+            return RedirectToAction("Show", "Addresses", new {domainID = id});
+        }
+
+        public ActionResult Anchors(long id)
+        {
+            return RedirectToAction("Show", "Anchors", new {domainID = id});
+        }
+
+        public ActionResult Certificates(long id)
+        {
+            return RedirectToAction("Show", "Certificates", new {domainID = id});
+        }
+
+        public ActionResult Add()
+        {
+            return View(new DomainModel());
+        }
+
+        [HttpPost]
+        public ActionResult Add(FormCollection formValues)
+        {
+            var domain = new DomainModel();
+
+            if (TryUpdateModel(domain))
+            {
+                var newDomain = Repository.Add(new Domain(domain.Name));
+
+                return RedirectToAction("Details", new {id = newDomain.ID});
+            }
+
+            return View(domain);
+        }
+
+        public ActionResult Delete(long id)
+        {
+            var domain = Repository.Get(id);
+            if (domain == null) return View("NotFound");
+
+            return View(domain);
+        }
+        
+        [HttpPost]
+        public ActionResult Delete(long id, string confirmButton)
+        {
+            var domain = Repository.Get(id);
+            if (domain == null) return View("NotFound");
+
+            Repository.Delete(domain);
+
+            return View("Deleted");
+        }
+
+        public ActionResult Disable(long id)
+        {
+            return EnableDisable(id, EntityStatus.Disabled);
+        }
+
+        public ActionResult Enable(long id)
+        {
+            return EnableDisable(id, EntityStatus.Enabled);
+        }
+
+        private ActionResult EnableDisable(long id, EntityStatus status)
+        {
+            var domain = Repository.Get(id);
+            if (domain == null) return View("NotFound");
+
+            domain.Status = status;
+            Repository.Update(domain);
+
+            return View("Details", domain);
+        }
+    }
+}
---
+++ csharp/admin/AdminMvc/Views/Certificates/Index.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<IEnumerable<Health.Direct.Config.Store.Certificate>>" %>
+<%@ Import Namespace="MvcContrib.UI.Pager"%>
+<%@ Import Namespace="MvcContrib.Pagination"%>
+<%@ Import Namespace="MvcContrib.UI.Grid"%>
+<%@ Import Namespace="AdminMvc.Controllers"%>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	Certificates
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Certificates</h2>
+
+    <%= Html.Grid(Model)
+        .Columns(
+            column =>
+                {
+                    column.For(d => d.Owner);
+                    column.For(d => d.Thumbprint);
+                    column.For(d => d.CreateDate);
+                    column.For(d => d.ValidStartDate);
+                    column.For(d => d.ValidEndDate);
+                    column.For(d => d.HasData);
+                    column.For(d => d.Status);
+                })%>
+    <%= Html.Pager((IPagination)Model) %>
+
+</asp:Content>
