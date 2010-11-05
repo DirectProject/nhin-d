53a74a19602b522c51cb1f8cb393e1de6edfe91d
Greg Meyer <gm2552@cerner.com>
2010-11-05 08:59:25 -0500

    Fixed issue with James bridge where it was not removing rejected recipients from the RCTP header.  It is, however, properly removing rejected recipients from the other routing headers.  This was causing rejected recipients to receive encrypted messages.

---
+++ csharp/gateway/smtpEventHandler/smtpEventHandler.cpp
@@ -0,0 +1,85 @@
+/* 
+ Copyright (c) 2010, Direct Project
+ All rights reserved.
+
+ Authors:
+    Umesh Madan     umeshma@microsoft.com
+  
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of The Direct Project (directproject.org) nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+// smtpEventHandler.cpp : Implementation of DLL Exports.
+
+
+#include "stdafx.h"
+#include "resource.h"
+#include "smtpEventHandler_i.h"
+#include "dllmain.h"
+
+// Used to determine whether the DLL can be unloaded by OLE
+STDAPI DllCanUnloadNow(void)
+{
+    return _AtlModule.DllCanUnloadNow();
+}
+
+
+// Returns a class factory to create an object of the requested type
+STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
+{
+    return _AtlModule.DllGetClassObject(rclsid, riid, ppv);
+}
+
+
+// DllRegisterServer - Adds entries to the system registry
+STDAPI DllRegisterServer(void)
+{
+    // registers object, typelib and all interfaces in typelib
+    HRESULT hr = _AtlModule.DllRegisterServer();
+	return hr;
+}
+
+
+// DllUnregisterServer - Removes entries from the system registry
+STDAPI DllUnregisterServer(void)
+{
+	HRESULT hr = _AtlModule.DllUnregisterServer();
+	return hr;
+}
+
+// DllInstall - Adds/Removes entries to the system registry per user
+//              per machine.	
+STDAPI DllInstall(BOOL bInstall, LPCWSTR pszCmdLine)
+{
+    HRESULT hr = E_FAIL;
+    static const wchar_t szUserSwitch[] = _T("user");
+
+    if (pszCmdLine != NULL)
+    {
+    	if (_wcsnicmp(pszCmdLine, szUserSwitch, _countof(szUserSwitch)) == 0)
+    	{
+    		AtlSetPerUserRegistration(true);
+    	}
+    }
+
+    if (bInstall)
+    {	
+    	hr = DllRegisterServer();
+    	if (FAILED(hr))
+    	{	
+    		DllUnregisterServer();
+    	}
+    }
+    else
+    {
+    	hr = DllUnregisterServer();
+    }
+
+    return hr;
+}
+
+
---
+++ csharp/windows services/dnsResponder.winsrv/DnsResponderWinSrv.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;using System.Configuration;
+
+using Health.Direct.DnsResponder;
+using Health.Direct.Config.Store;
+
+namespace Health.Direct.DnsResponder.WinSrv
+{
+    public partial class DnsResponderWinSrv : ServiceBase
+    {
+        protected DnsResponderTCP m_dnsResponderTCP = null;
+        protected DnsResponderUDP m_dnsResponderUDP = null;
+        protected DnsServer m_dnsServer = null;
+        protected DBStore m_store = null;
+
+        public DnsResponderWinSrv()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// method to initialize fields utilized by the service
+        /// </summary>
+        protected void InitializeService()
+        {
+            
+           //----------------------------------------------------------------------------------------------------
+           //---TODO: load this from configuration
+           DnsServerSettings settings = new DnsServerSettings();
+            
+            //----------------------------------------------------------------------------------------------------
+            //---create the DNS Server instance
+            m_dnsServer = new DnsServer(new DBStore()
+                , settings);
+
+            //----------------------------------------------------------------------------------------------------
+            //---init the tcp and udp responders
+            m_dnsResponderTCP = new DnsResponderTCP(m_dnsServer);
+            m_dnsResponderUDP = new DnsResponderUDP(m_dnsServer);
+        }
+
+        public void StartService(string[] args)
+        {
+            InitializeService();
+            m_dnsResponderTCP.Start();
+            m_dnsResponderUDP.Start();
+        }
+
+        public void StopService()
+        {
+
+            m_dnsResponderTCP.Stop();
+            m_dnsResponderUDP.Stop();
+            
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.StartService(args);
+        }
+
+        protected override void OnStop()
+        {
+            this.StopService();
+        }
+    }
+}
