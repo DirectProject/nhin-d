0c517c6a2f9fa144e10d1528c04792f3d247fe99
Mark Bamberg <bambergm@gmail.com>
2010-11-05 16:30:56 -0400

    Make setdomain.sh rerunable Remove metro

--- java/install/src/scripts/setdomain.sh
+++ java/install/src/scripts/setdomain.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -x
-
-sed -i "s/<servername>localhost<\/servername>/<servername>$1<\/servername>/g" $DIRECT_HOME/james-2.3.2/apps/james/SAR-INF/config.xml
-sed -i "s/<postmaster>Postmaster@localhost<\/postmaster>/<postmaster>postmaster@$1<\/postmaster>/g" $DIRECT_HOME/james-2.3.2/apps/james/SAR-INF/config.xml
-sed -i "s/RecipAndSenderIsNotLocal=mydomain.com/RecipAndSenderIsNotLocal=$1/g" $DIRECT_HOME/james-2.3.2/apps/james/SAR-INF/config.xml
-
+export DIRECT_DOMAIN=$1
+ant -q -f $DIRECT_HOME/james-2.3.2/bin/set-domain.xml
 echo "Domain set to $1"
 echo "Postmaster set to postmaster@$1"
