edd0013d4a15975822b7ead311c153f453276925
John Theisen <john.theisen@kryptiq.com>
2010-11-11 11:03:21 -0800

    Adjusted the AssemblyInfo.cs files to have the shared attributes located in the GlobalAssemblyInfo.cs file so that it is easier to stamp all assemblies with the same build number at install time.

--- csharp/tools/toolutil/Properties/AssemblyInfo.cs
+++ csharp/tools/toolutil/Properties/AssemblyInfo.cs
@@ -5,14 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nhinToolUtil")]
+[assembly: AssemblyTitle("Direct Config Tools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Direct Project")]
-[assembly: AssemblyProduct("nhinToolUtil")]
-[assembly: AssemblyCopyright("Copyright © Direct Project")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5890aeed-93e0-445b-9047-13d2669a319e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
