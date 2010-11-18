f225602873c5d77dfa01c00015a452dc007ad5d7
Greg Meyer <gm2552@cerner.com>
2010-11-18 13:42:11 -0600

    Added Guice modules, providers, and annotations for DNS server. Added logic for correct keytag creation in cert store. Corrected issue with CERT DNS resolver using "." instead of "@" when doing lookups. Added DNS test for CERT and fixed bugs associated with those tests. Added MX record tests.

--- java/dns/src/test/java/org/nhindirect/dns/util/DNSRecordUtil.java
+++ java/dns/src/test/java/org/nhindirect/dns/util/DNSRecordUtil.java
@@ -1,13 +1,24 @@
 package org.nhindirect.dns.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.security.auth.x500.X500Principal;
+
+import org.apache.commons.io.FileUtils;
 import org.nhind.config.DnsRecord;
 import org.nhindirect.config.store.DNSRecord;
 import org.nhindirect.config.store.util.DNSRecordUtils;
 
 public class DNSRecordUtil 
 {
+	private static final String certBasePath = "src/test/resources/certs/"; 
+	
 	private static DnsRecord toDnsRecord(DNSRecord rec)
 	{
 		DnsRecord retVal = new DnsRecord();
@@ -20,6 +31,52 @@ public class DNSRecordUtil
 		
 		return retVal;
 	}
+
+	public static String getCertOwner(X509Certificate cert)
+    {
+		X500Principal prin = cert.getSubjectX500Principal();
+		
+    	// get the domain name
+		Map<String, String> oidMap = new HashMap<String, String>();
+		oidMap.put("1.2.840.113549.1.9.1", "EMAILADDRESS");  // OID for email address
+		String prinName = prin.getName(X500Principal.RFC1779, oidMap);    
+		
+		// see if there is an email address first in the DN
+		String searchString = "EMAILADDRESS=";
+		int index = prinName.indexOf(searchString);
+		if (index == -1)
+		{
+			searchString = "CN=";
+			// no Email.. check the CN
+			index = prinName.indexOf(searchString);
+			if (index == -1)
+				return ""; // no CN... nothing else that can be done from here
+		}
+		
+		// look for a "," to find the end of this attribute
+		int endIndex = prinName.indexOf(",", index);
+		String address;
+		if (endIndex > -1)
+			address = prinName.substring(index + searchString.length(), endIndex);
+		else 
+			address= prinName.substring(index + searchString.length());
+		
+		return address;
+    }	
+	
+	public static X509Certificate loadCertificate(String certFileName) throws Exception
+	{
+		File fl = new File(certBasePath + certFileName);
+		
+		InputStream str = new ByteArrayInputStream(FileUtils.readFileToByteArray(fl));
+		
+		X509Certificate retVal = (X509Certificate)CertificateFactory.getInstance("X.509").generateCertificate(str);
+		
+		str.close();
+		
+		return retVal;
+	}
+	
 	
 	public static DnsRecord createARecord(String name, String ip) throws Exception
 	{
@@ -34,4 +91,11 @@ public class DNSRecordUtil
 		
 		return toDnsRecord(rec);
 	}
+	
+	public static DnsRecord createMXRecord(String name, String target, int priority) throws Exception
+	{
+		DNSRecord rec = DNSRecordUtils.createMXRecord(name, target, 86400L, priority);
+		
+		return toDnsRecord(rec);
+	}	
 }
