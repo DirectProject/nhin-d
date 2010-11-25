59861448ac080d77e1b854554e416ca6e17ae30a
John Theisen <john.theisen@kryptiq.com>
2010-11-24 17:03:43 -0800

    Advancing version number to 1.0.0.2

--- csharp/GlobalAssemblyInfo.cs
+++ csharp/GlobalAssemblyInfo.cs
@@ -6,5 +6,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
