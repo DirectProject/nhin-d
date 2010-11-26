97c25bda627fd694f7fd46c7b684ba04e64023da
Beau Grantham <bgrantha@harris.com>
2010-11-26 10:27:27 -0500

    Javadoc

--- java/xd-common/src/main/java/org/nhindirect/xd/proxy/DocumentRepositoryProxy.java
+++ java/xd-common/src/main/java/org/nhindirect/xd/proxy/DocumentRepositoryProxy.java
@@ -1,3 +1,31 @@
+/* 
+ * Copyright (c) 2010, NHIN Direct Project
+ * All rights reserved.
+ *  
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution.  
+ * 3. Neither the name of the the NHIN Direct Project (nhindirect.org)
+ *    nor the names of its contributors may be used to endorse or promote products 
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY 
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.nhindirect.xd.proxy;
 
 import ihe.iti.xds_b._2007.DocumentRepositoryPortType;
@@ -21,7 +49,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * 
+ * Wrapper class for handling calls to the Document Repository WS (XD*). The
+ * initialization uses a WSDL located within the JAR for convenience. The
+ * constructor requires and endpoint and an optional HandlerResolver.
  * 
  * @author beau
  */
@@ -35,21 +65,26 @@ public class DocumentRepositoryProxy
     private static final Log LOGGER = LogFactory.getFactory().getInstance(DocumentRepositoryProxy.class);
 
     /**
+     * Construct a new proxy using the provided endpoint.
+     * 
      * @param endpoint
-     * @throws Exception
+     *            The endpoint of the service.
      */
-    public DocumentRepositoryProxy(String endpoint) throws Exception
+    public DocumentRepositoryProxy(String endpoint)
     {
         this.endpoint = endpoint;
         initProxy();
     }
 
     /**
+     * Construct a new proxy using the provided endpoint and a HandlerResolver.
+     * 
      * @param endpoint
+     *            The endpoint of the service.
      * @param handlerResolver
-     * @throws Exception
+     *            The HandlerResolver to attach.
      */
-    public DocumentRepositoryProxy(String endpoint, HandlerResolver handlerResolver) throws Exception
+    public DocumentRepositoryProxy(String endpoint, HandlerResolver handlerResolver)
     {
         this(endpoint);
         this.handlerResolver = handlerResolver;
@@ -84,12 +119,15 @@ public class DocumentRepositoryProxy
     }
 
     /**
+     * Wrapper method to call the
+     * documentRepositoryProvideAndRegisterDocumentSetB
+     * (ProvideAndRegisterDocumentSetRequestType) service method.
+     * 
      * @param body
-     * @return
-     * @throws Exception
+     *            The ProvideAndRegisterDocumentSetRequestType object.
+     * @return a RegistryResponseType.
      */
     public RegistryResponseType provideAndRegisterDocumentSetB(ProvideAndRegisterDocumentSetRequestType body)
-            throws Exception
     {
         if (proxy == null)
             initProxy();
@@ -99,8 +137,12 @@ public class DocumentRepositoryProxy
     }
 
     /**
+     * Wrapper method to call the
+     * retrieveDocumentSet(RetrieveDocumentSetRequestType) service method.
+     * 
      * @param body
-     * @return
+     *            The RetrieveDocumentSetRequestType object.
+     * @return a RetrieveDocumentSetResponseType.
      * @throws Exception
      */
     public RetrieveDocumentSetResponseType retrieveDocumentSet(RetrieveDocumentSetRequestType body) throws Exception
@@ -110,5 +152,4 @@ public class DocumentRepositoryProxy
 
         throw new OperationNotSupportedException("Not implemented.");
     }
-
 }
