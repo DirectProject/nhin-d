7f1d23cda50d3f1d7b12cc27c9e43083deb7279f
Beau Grantham <bgrantha@harris.com>
2010-11-30 09:29:12 -0500

    1. Refactor SOAP Handler classes. 2. Move ThreadData to new SOAP package. 3. Fixed bug with duplicate mailto: in URIs. 4. Add stubs for direct:metadata-level header.

--- java/xd-common/src/main/java/org/nhindirect/xd/soap/DirectSOAPHandlerResolver.java
+++ java/xd-common/src/main/java/org/nhindirect/xd/soap/DirectSOAPHandlerResolver.java
@@ -26,65 +26,33 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.nhind.mail.service;
+package org.nhindirect.xd.soap;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.ws.handler.Handler;
+import javax.xml.ws.handler.HandlerResolver;
 import javax.xml.ws.handler.PortInfo;
 
-import junit.framework.TestCase;
-
 /**
- * Test class for methods in RepositoryHandlerResolver.
+ * Message handler resolver.
  * 
- * @author beau
+ * @author Vincent Lewis
  */
-public class RepositoryHandlerResolverTest extends TestCase {
-
-    /**
-     * Constructor
-     * 
-     * @param testName
-     *            The test name
-     */
-    public RepositoryHandlerResolverTest(String testName) {
-        super(testName);
-    }
+public class DirectSOAPHandlerResolver implements HandlerResolver
+{
 
     /*
      * (non-Javadoc)
      * 
-     * @see junit.framework.TestCase#setUp()
+     * @see javax.xml.ws.handler.HandlerResolver#getHandlerChain(javax.xml.ws.handler.PortInfo)
      */
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    public List<Handler> getHandlerChain(PortInfo portInfo)
+    {
+        List<Handler> handlerList = new ArrayList<Handler>();
+        handlerList.add(new DirectSOAPHandler());
+        return handlerList;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see junit.framework.TestCase#tearDown()
-     */
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-
-    /**
-     * Test the getHandlerChain method.
-     */
-    public void testGetHandlerChain() {
-        PortInfo portInfo = null;
-        List<Handler> output = null;
-        RepositoryHandlerResolver handler = new RepositoryHandlerResolver();
-
-        output = handler.getHandlerChain(portInfo);
-
-        assertNotNull("List is null", output);
-        assertTrue("List contains 0 elements", !output.isEmpty());
-        assertEquals("List contains more than expected elements", 1, output.size());
-        assertTrue("List does not contain expected element", output.get(0) instanceof RepositorySOAPHandler);
-    }
 }
