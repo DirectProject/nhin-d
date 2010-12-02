00888dca2ce0901d7b70272fe6708d2661e42ed2
John Theisen <john.theisen@kryptiq.com>
2010-12-02 10:34:01 -0800

    Moved the build-installer to the installer folder. The setting of the version number is now automated.

--- csharp/installer/install-dev.bat
+++ csharp/installer/install-dev.bat
@@ -14,7 +14,6 @@ if %ERRORLEVEL% NEQ 0 goto :error
 echo Succeeded
 
 echo Setting up EventLog sources...
-@rem for /F "eol=; tokens=1,2* delims=," %%i in (event-sources.txt) do eventcreate /ID 1 /L APPLICATION /T INFORMATION /SO %%i /D "%%j"
 powershell -Command "set-executionpolicy unrestricted -force"
 for /F "eol=; tokens=1,2* delims=," %%i in (event-sources.txt) do powershell -File createeventlogsource.ps1 %%i "%%j"
 if %ERRORLEVEL% NEQ 0 goto :error
