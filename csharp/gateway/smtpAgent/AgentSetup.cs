aa99a33fcc4d8b40ac1603b9d73a65fd14601f6c
Greg Meyer <gm2552@cerner.com>
2011-01-03 14:52:14 -0600

    Certificates are properly looked up using alternative subject names first.  Added tests. TrustChainValidator properly validates self signed certificates.  Added tests. LDAP cert store can not retrieve all certs.  Added tests.

---
+++ csharp/gateway/smtpAgent/AgentSetup.cs
@@ -0,0 +1,51 @@
+﻿/* 
+ Copyright (c) 2010, Direct Project
+ All rights reserved.
+
+ Authors:
+    Umesh Madan     umeshma@microsoft.com
+  
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of The Direct Project (directproject.org) nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+using System.IO;
+using System.Runtime.InteropServices;
+
+using Health.Direct.Common.Certificates;
+
+namespace Health.Direct.SmtpAgent
+{
+    /// <summary>
+    /// COM object used by scripting components. Aids in Agent setup
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("A1F86888-959C-49e1-B786-C17465668972")]
+    [ClassInterface(ClassInterfaceType.AutoDispatch)]
+    public class AgentSetup
+    {
+        public AgentSetup()
+        {
+        }
+        
+        public void EnsureStandardMachineStores()
+        {
+            SystemX509Store.CreateAll();
+        }
+        
+        public void ValidateConfig(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException();
+            }
+            
+            SmtpAgentSettings settings = SmtpAgentSettings.LoadSettings(filePath);
+            settings.Validate();
+        }
+    }
+}
