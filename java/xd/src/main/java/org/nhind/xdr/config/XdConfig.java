80f35136745aa60315761b2e40e8cc91db8b4015
Beau Grantham <bgrantha@harris.com>
2011-01-04 10:11:27 -0500

    1. Added default values for some XD settings. 2. Javadoc. 3. Updated James config.xml to include XD flow.

--- java/xd/src/main/java/org/nhind/xdr/config/XdConfig.java
+++ java/xd/src/main/java/org/nhind/xdr/config/XdConfig.java
@@ -1,14 +1,56 @@
+/* 
+ * Copyright (c) 2010, NHIN Direct Project
+ * All rights reserved.
+ *  
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution.  
+ * 3. Neither the name of the the NHIN Direct Project (nhindirect.org)
+ *    nor the names of its contributors may be used to endorse or promote products 
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY 
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.nhind.xdr.config;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nhind.config.ConfigurationServiceProxy;
 import org.nhind.config.Setting;
 
+/**
+ * Configuration class for XD related operations.
+ * 
+ * @author beau
+ */
 public class XdConfig
 {
-    private enum Keys
+    private static final Log LOGGER = LogFactory.getFactory().getInstance(XdConfig.class);
+    
+    /**
+     * Enumeration representing valid XD settings and default values.
+     * 
+     * @author beau
+     */
+    private enum Settings
     {
-        MAIL_HOST("xd.MailHost"), 
-        MAIL_USER("xd.MailUser"), 
+        MAIL_HOST("xd.MailHost", "localhost"), 
+        MAIL_USER("xd.MailUser", "direct"), 
         MAIL_PASS("xd.MailPass"), 
         AUDIT_METHOD("xd.AuditMethod"), 
         AUDIT_HOST("xd.AuditHost"), 
@@ -16,15 +58,37 @@ public class XdConfig
         AUDIT_FILE("xd.AuditFile");
 
         private String key;
+        private String dflt;
 
-        private Keys(String key)
+        private Settings(String key)
         {
             this.key = key;
         }
 
+        private Settings(String key, String dflt)
+        {
+            this(key);
+            this.dflt = dflt;
+        }
+
+        /**
+         * Return the value of key.
+         * 
+         * @return the value of key.
+         */
         public String getKey()
         {
             return key;
+        }
+
+        /**
+         * Return the value of dflt.
+         * 
+         * @return the value of dflt.
+         */
+        public String getDefault()
+        {
+            return dflt;
         }
     }
 
@@ -38,64 +102,77 @@ public class XdConfig
 
     private ConfigurationServiceProxy proxy;
 
+    /**
+     * Default constructor.
+     */
     public XdConfig()
     {
         this.init();
     }
-    
+
+    /**
+     * Constructor which takes a configuration-service endpoint.
+     * 
+     * @param endpoint
+     *            The endpoint for the configuration-service.
+     */
     public XdConfig(String endpoint)
     {
         this.proxy = new ConfigurationServiceProxy(endpoint);
         this.init();
     }
 
+    /**
+     * Constructor which takes a ConfigurationServiceProxy.
+     * 
+     * @param proxy
+     *            The ConfigurationServiceProxy object.
+     */
     public XdConfig(ConfigurationServiceProxy proxy)
     {
         this.proxy = proxy;
         this.init();
     }
 
+    /**
+     * Initialize the configuration object.
+     */
     public void init()
     {
         if (proxy == null)
             return;
 
-        Setting tmp;
-
         try
         {
-            tmp = proxy.getSettingByName(Keys.MAIL_HOST.getKey());
-            if (tmp != null)
-                mailHost = tmp.getValue();
-
-            tmp = proxy.getSettingByName(Keys.MAIL_USER.getKey());
-            if (tmp != null)
-                mailUser = tmp.getValue();
-
-            tmp = proxy.getSettingByName(Keys.MAIL_PASS.getKey());
-            if (tmp != null)
-                mailPass = tmp.getValue();
-
-            tmp = proxy.getSettingByName(Keys.AUDIT_METHOD.getKey());
-            if (tmp != null)
-                auditMethod = tmp.getValue();
-
-            tmp = proxy.getSettingByName(Keys.AUDIT_HOST.getKey());
-            if (tmp != null)
-                auditHost = tmp.getValue();
-
-            tmp = proxy.getSettingByName(Keys.AUDIT_PORT.getKey());
-            if (tmp != null)
-                auditPort = tmp.getValue();
-
-            tmp = proxy.getSettingByName(Keys.AUDIT_FILE.getKey());
-            if (tmp != null)
-                auditFile = tmp.getValue();
+            mailHost = getValue(Settings.MAIL_HOST);
+            mailUser = getValue(Settings.MAIL_USER);
+            mailPass = getValue(Settings.MAIL_PASS);
+            auditMethod = getValue(Settings.AUDIT_METHOD);
+            auditHost = getValue(Settings.AUDIT_HOST);
+            auditPort = getValue(Settings.AUDIT_PORT);
+            auditFile = getValue(Settings.AUDIT_FILE);
         }
         catch (Exception e)
         {
-
+            LOGGER.error("Error initializing the XD configuration");
         }
+    }
+    
+    private String getValue(Settings xdSetting) throws Exception
+    {
+        Setting tmp;
+        String value = null;
+        
+        if (proxy != null)
+        {
+            tmp = proxy.getSettingByName(xdSetting.getKey());
+            if (tmp != null)
+                value = tmp.getValue();
+            else
+                value = xdSetting.getDefault();
+        }
+        
+        return value;
     }
 
     /**
