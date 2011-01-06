3cad970f9bdcb25a30c96247bbcf3dddf25e7b23
timjeffcoat <timjeffcoat@gmail.com>
2011-01-06 08:12:05 -0700

    config ui layered the mx form in dns to prevent tls from going out of the field box.

---
+++ csharp/xd/ExternalIdentifier.cs
@@ -0,0 +1,44 @@
+﻿/* 
+ Copyright (c) 2010, Direct Project
+ All rights reserved.
+
+ Authors:
+    Arien Malec     arien.malec@nhindirect.org
+  
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of The Direct Project (directproject.org) nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+using System.Xml.Linq;
+
+namespace Health.Direct.Xd
+{
+    /// <summary>
+    /// Element represeting a patient identifier in IHE XD* ebXML
+    /// </summary>
+    public class ExternalIdentifier : XElement
+    {
+        /// <summary>
+        /// Initializes an instance with the supplied patient ID
+        /// </summary>
+        public ExternalIdentifier(string scheme, string value, string name)
+            : base("ExternalIdentifier")
+        {
+            Initialize(scheme, value, name);
+        }
+
+        private void Initialize(string scheme, string value, string name)
+        {
+            this.Add(new XAttribute("identificationScheme", scheme),
+                     new XAttribute("value", value),
+                     new XAttribute("objectType", XDMetadataStandard.ExternalIdentifierObjectType),
+                     new Name(name));
+        }
+
+
+    }
+}
