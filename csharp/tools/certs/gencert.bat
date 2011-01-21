a110b062eed7237a1b34fec54364e476edb03711
umeshma <umeshma@microsoft.com>
2011-01-20 16:53:37 -0800

    Improvements to  genca.bat - Set Email usage OID - More complete Distinguished name

--- csharp/tools/certs/gencert.bat
+++ csharp/tools/certs/gencert.bat
@@ -13,7 +13,8 @@ call :MakeCert %1 %2 %3
 goto :Done
 
 :MakeCert
-makecert -pe -n "CN=%cn%" -ss my -sr LocalMachine -a sha1 -sky signature -in "%ca%" -is MY -cy end -ir LocalMachine %3\%cn%.cer -m 18
+makecert -pe -n "CN=%cn%,E=%cn%" -ss my -sr LocalMachine -a sha1 -sky signature -in "%ca%" -is MY -cy end -ir LocalMachine %3\%cn%.cer -m 18 -eku "1.3.6.1.5.5.7.3.4"
+
 goto :EOF
 
 :Usage
@@ -29,6 +30,8 @@ echo Your private key is automatically saved to your LocalMachine Personal Store
 echo. 
 echo Your CA certificate must be in your Local Machine "Personal" certificate store
 echo If you used genca.bat to generate your CA, it was automatically placed there
+echo.
+echo The certificate usage is set to Email
 echo.
 echo *************************************
 goto :Done
