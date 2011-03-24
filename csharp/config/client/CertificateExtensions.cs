b419091cdc6e2e3343153c6b9e2146ed90e69262
umeshma <umeshma@microsoft.com>
2011-03-24 16:17:11 -0700

    1. Bug Fixes 2. Unit Tests

---
+++ csharp/config/client/CertificateExtensions.cs
@@ -0,0 +1,167 @@
+﻿/* 
+ Copyright (c) 2010, Direct Project
+ All rights reserved.
+
+ Authors:
+    Umesh Madan     umeshma@microsoft.com
+  
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of The Direct Project (directproject.org) nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+using Health.Direct.Common.Certificates;
+using Health.Direct.Common.Extensions;
+using Health.Direct.Config.Client.CertificateService;
+using Health.Direct.Config.Store;
+
+namespace Health.Direct.Config.Client
+{
+    public static class CertificateExtensions
+    {
+        internal static CertificateGetOptions FullCertData = new CertificateGetOptions
+                                                                 {
+                                                                     IncludeData = true,
+                                                                     IncludePrivateKey = true
+                                                                 };
+
+        internal static CertificateGetOptions CertInfo = new CertificateGetOptions
+                                                             {
+                                                                 IncludeData = false,
+                                                                 IncludePrivateKey = false
+                                                             };
+        
+        //public static void AddCertificate(this CertificateStoreClient client, Certificate cert)
+        //{
+        //    if (cert == null)
+        //    {
+        //        throw new ArgumentNullException("cert");  
+        //    }
+            
+        //    client.AddCertificates(new Certificate[] {cert});
+        //}
+
+        public static bool Contains(this CertificateStoreClient client, X509Certificate2 certificate)
+        {
+            if (certificate == null)
+            {
+                throw new ArgumentNullException("certificate");
+            }
+
+            return client.Contains(certificate.ExtractEmailNameOrName(), certificate.Thumbprint);
+        }
+
+        public static bool Contains(this CertificateStoreClient client, string owner, string thumbprint)
+        {
+            if (string.IsNullOrEmpty(owner))
+            {
+                throw new ArgumentException("value was null or empty", "owner");
+            }
+            if (string.IsNullOrEmpty(thumbprint))
+            {
+                throw new ArgumentException("value was null or empty", "thumbprint");
+            }
+
+            Certificate cert = client.GetCertificate(owner, thumbprint, CertificateExtensions.CertInfo);
+            return (cert != null);
+        }
+
+        public static Certificate GetCertificate(this CertificateStoreClient client, string owner, string thumbprint)
+        {
+            return client.GetCertificate(owner, thumbprint, FullCertData);
+        }
+        
+        public static Certificate GetCertificate(this CertificateStoreClient client, long certificateID, CertificateGetOptions options)
+        {
+            Certificate[] certs = client.GetCertificates(new long[] {certificateID}, options);
+            if (certs.IsNullOrEmpty())
+            {
+                return null;
+            }
+            
+            return certs[0];
+        }
+        
+        public static Certificate[] GetCertificatesForOwner(this CertificateStoreClient client, string owner)
+        {
+            return client.GetCertificatesForOwner(owner, FullCertData);
+        }
+
+        public static Certificate[] GetCertificatesForOwner(this CertificateStoreClient client, string owner, EntityStatus status)
+        {
+            CertificateGetOptions options = FullCertData.Clone();
+            options.Status = status;
+            return client.GetCertificatesForOwner(owner, options);
+        }
+        
+        public static X509Certificate2Collection GetX509CertificatesForOwner(this CertificateStoreClient client, string owner)
+        {
+            return Certificate.ToX509Collection(client.GetCertificatesForOwner(owner));
+        }
+        
+        public static void RemoveCertificate(this CertificateStoreClient client, long certificateID)
+        {
+            client.RemoveCertificates(new long[] {certificateID});
+        }
+        
+        public static IEnumerable<Certificate> EnumerateCertificates(this CertificateStoreClient client, int chunkSize)
+        {
+            return client.EnumerateCertificates(chunkSize, FullCertData);
+        }
+
+        public static IEnumerable<Certificate> EnumerateCertificates(this CertificateStoreClient client, int chunkSize, CertificateGetOptions options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException("options");
+            }
+
+            if (chunkSize < 1)
+            {
+                throw new ArgumentException("value was less than 1", "chunkSize");
+            }
+
+            long lastID = -1;
+
+            Certificate[] certs;
+            while (true)
+            {
+                certs = client.EnumerateCertificates(lastID, chunkSize, options);
+                if (certs.IsNullOrEmpty())
+                {
+                    yield break;
+                }
+                
+                for (int i = 0; i < certs.Length; ++i)
+                {
+                    yield return certs[i];
+                }
+                lastID = certs[certs.Length - 1].ID;
+            }
+        }
+        
+        public static IEnumerable<X509Certificate2> EnumerateX509Certificates(this CertificateStoreClient client, int chunkSize)
+        {
+            foreach(Certificate cert in client.EnumerateCertificates(chunkSize, FullCertData))
+            {
+                yield return cert.ToX509Certificate();
+            }
+        }
+        
+        public static CertificateGetOptions Clone(this CertificateGetOptions options)
+        {
+            return new CertificateGetOptions {
+                                                 IncludeData = options.IncludeData,
+                                                 IncludePrivateKey = options.IncludePrivateKey,
+                                                 Status = options.Status
+                                             };
+        }
+    }
+}
---
+++ java/tags/gateway-1.0/src/test/java/org/nhindirect/gateway/smtp/DefaultSmtpAgent_ProcessMessage_Test.java
@@ -0,0 +1,279 @@
+package org.nhindirect.gateway.smtp;
+
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.mail.Message.RecipientType;
+
+import org.nhindirect.gateway.smtp.config.SmtpAgentConfig;
+import org.nhindirect.gateway.smtp.module.SmtpAgentConfigModule;
+import org.nhindirect.gateway.testutils.TestUtils;
+import org.nhindirect.gateway.testutils.BaseTestPlan;
+import org.nhindirect.stagent.mail.Message;
+import org.nhindirect.stagent.mail.notifications.NotificationHelper;
+import org.nhindirect.stagent.mail.notifications.NotificationMessage;
+import org.nhindirect.stagent.provider.MockNHINDAgentProvider;
+import org.nhindirect.stagent.utils.InjectionUtils;
+import org.nhindirect.stagent.DefaultMessageEnvelope;
+import org.nhindirect.stagent.NHINDAgent;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Provider;
+import com.google.inject.name.Names;
+
+import junit.framework.TestCase;
+
+public class DefaultSmtpAgent_ProcessMessage_Test extends TestCase 
+{
+	
+	abstract class TestPlan extends BaseTestPlan 
+	{
+		protected SmtpAgent agent;
+		protected Collection<Module> modules = new ArrayList<Module>();
+		protected Provider<NHINDAgent> agentProvider;
+		
+		@Override
+		public void setupMocks()
+		{
+			String configfile = TestUtils.getTestConfigFile(getConfigFileName());
+			
+			if (agentProvider == null)
+				agentProvider = new MockNHINDAgentProvider(Arrays.asList(new String[] {"cerner.com", "securehealthemail.com"}));
+			
+			try
+			{
+				modules.add(SmtpAgentConfigModule.create(new URL("file://" + configfile), null, agentProvider));	
+			}
+			catch (Exception e)
+			{
+				throw new RuntimeException(e);
+			}
+			
+			Injector injector = Guice.createInjector(modules);
+			SmtpAgentConfig config = injector.getInstance(SmtpAgentConfig.class);
+			agent = config.getAgentInjector().getInstance(SmtpAgent.class);
+			
+			injector.injectMembers(agent.getAgent());
+			injector.injectMembers(agent);
+		}
+		
+		@Override
+		protected void performInner() throws Exception 
+		{
+			DefaultMessageEnvelope env = new DefaultMessageEnvelope(getMessageToProcess());
+			
+			MessageProcessResult result = agent.processMessage(env.getMessage(), env.getRecipients(), env.getSender());			
+			doAssertions(result);
+		}	
+	
+		protected void doAssertions(MessageProcessResult result) throws Exception
+		{
+		}
+		
+		
+		protected String getConfigFileName()
+		{
+			return "ValidConfig.xml";
+		}
+				
+		protected abstract String getMessageToProcess() throws Exception;	
+	}
+	
+	public void testProcessValidIncomingMessage_AssertSuccessfulResultWithNoBounces() throws Exception 
+	{
+		new TestPlan() 
+		{			
+			protected String getMessageToProcess() throws Exception
+			{
+				return TestUtils.readMessageResource("PlainIncomingMessage.txt");
+			}	
+			
+			@Override
+			protected void doAssertions(MessageProcessResult result) throws Exception
+			{
+				assertNotNull(result);
+				assertNotNull(result.getProcessedMessage());
+			}
+			
+		}.perform();
+	}
+	
+	public void testProcessValidIncomingMessageWithMDNRequest_AssertSuccessfulResultWithNoBounces() throws Exception 
+	{
+		new TestPlan() 
+		{			
+			protected String getMessageToProcess() throws Exception
+			{
+				return TestUtils.readMessageResource("PlainIncomingMessage.txt");
+			}	
+			
+			@Override
+			protected void performInner() throws Exception 
+			{
+				DefaultMessageEnvelope env = new DefaultMessageEnvelope(getMessageToProcess());
+				
+				// add the notification request
+				NotificationHelper.requestNotification(env.getMessage());
+				
+				MessageProcessResult result = agent.processMessage(env.getMessage(), env.getRecipients(), env.getSender());			
+				doAssertions(result);
+			}
+			
+			@Override
+			protected void doAssertions(MessageProcessResult result) throws Exception
+			{
+				assertNotNull(result);
+				assertNotNull(result.getProcessedMessage());
+				assertNotNull(result.getProcessedMessage().getMessage());
+				assertNotNull(result.getNotificationMessages());
+				assertTrue(result.getNotificationMessages().size() > 0);
+				
+				// get the first message
+				NotificationMessage notiMsg = result.getNotificationMessages().iterator().next();
+				
+				assertEquals(1, notiMsg.getRecipients(RecipientType.TO).length);
+				
+				Message processedMessage = result.getProcessedMessage().getMessage();
+				String processedSender = processedMessage.getFrom()[0].toString();
+				String notiRecip = notiMsg.getRecipients(RecipientType.TO)[0].toString();
+				// make sure the to and from are the same
+				assertEquals(processedSender, notiRecip);
+				
+			}
+			
+		}.perform();
+	}
+	
+	public void testProcessValidOutgoingMessageWithMDNRequest_AssertSuccessfulResult() throws Exception 
+	{
+		new TestPlan() 
+		{			
+			protected String getMessageToProcess() throws Exception
+			{
+				return TestUtils.readMessageResource("PlainOutgoingMessage.txt");
+			}	
+			
+			@Override
+			protected void performInner() throws Exception 
+			{
+				DefaultMessageEnvelope env = new DefaultMessageEnvelope(getMessageToProcess());
+				
+				// add the notification request
+				NotificationHelper.requestNotification(env.getMessage());
+				
+				MessageProcessResult result = agent.processMessage(env.getMessage(), env.getRecipients(), env.getSender());			
+				doAssertions(result);
+			}			
+			
+			@Override
+			protected void doAssertions(MessageProcessResult result) throws Exception
+			{
+				assertNotNull(result);
+				assertNotNull(result.getProcessedMessage());
+			}
+			
+		}.perform();
+	}	
+
+	public void testProcessValidOutgoingMessage_AssertSuccessfulResultWithNoBounces() throws Exception 
+	{
+		new TestPlan() 
+		{			
+			protected String getMessageToProcess() throws Exception
+			{
+				return TestUtils.readMessageResource("PlainOutgoingMessage.txt");
+			}	
+			
+			@Override
+			protected void doAssertions(MessageProcessResult result) throws Exception
+			{
+				assertNotNull(result);
+				assertNotNull(result.getProcessedMessage());
+			}
+			
+		}.perform();
+	}	
+	
+	public void testUniitializedNHINDAgent_AssertUninitializedException() throws Exception 
+	{
+		new TestPlan() 
+		{	
+			@Override
+			public void setupMocks()
+			{
+				super.setupMocks();
+				// a little bad magic to set the private agent to null
+				try
+				{
+					Field field = agent.getClass().getDeclaredField("agent");
+					field.setAccessible(true);
+					field.set(agent, null);
+				}
+				catch (Exception e)
+				{
+					throw new RuntimeException(e);
+				}
+			}
+			
+			protected String getMessageToProcess() throws Exception
+			{
+				return TestUtils.readMessageResource("PlainIncomingMessage.txt");
+			}	
+			
+			@Override
+			protected void assertException(Exception exception) throws Exception 
+			{
+				assertTrue(exception instanceof SmtpAgentException);
+				SmtpAgentException ex = (SmtpAgentException)exception;
+				assertEquals(SmtpAgentError.Uninitialized, ex.getError());
+			}
+
+			
+		}.perform();
+	}	
+	
+	public void testProcessValidOutgoingMessage_AssertSuccessfulResultWithOutgoingBounces() throws Exception 
+	{
+		new TestPlan() 
+		{			
+			
+			
+			@Override
+			public void setupMocks()
+			{
+				modules.add(new AbstractModule()
+				{
+					@Override
+					protected void configure()
+					{
+						bind(InjectionUtils.collectionOf(String.class)).
+						annotatedWith(Names.named("MockAgentAnchorDomains")).
+							toInstance(Arrays.asList(new String[] {"starugh-stateline.com"}));						
+					}
+				});
+				super.setupMocks();
+			}			
+			
+			protected String getMessageToProcess() throws Exception
+			{
+				return TestUtils.readMessageResource("PlainOutgoingMessageWithRejectedRecips.txt");
+			}	
+			
+			@Override
+			protected void doAssertions(MessageProcessResult result) throws Exception
+			{
+				assertNotNull(result);
+				assertNotNull(result.getProcessedMessage());
+			}
+			
+		}.perform();
+	}		
+	
+	
+}
