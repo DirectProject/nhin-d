e181649546315ca6a91aeff3065348ca0f6abe85
Greg Meyer <gm2552@cerner.com>
2011-03-25 06:39:24 -0500

    Committing agent 1.1.3 and gateway 1.2.1 tags

---
+++ java/tags/agent-1.1.3/src/test/java/org/nhindirect/stagent/cert/impl/AttributesAdapter.java
@@ -0,0 +1,53 @@
+package org.nhindirect.stagent.cert.impl;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+
+public class AttributesAdapter implements Attributes {
+
+	public Attribute get(String attrID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public NamingEnumeration<? extends Attribute> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public NamingEnumeration<String> getIDs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isCaseIgnored() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Attribute put(Attribute attr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Attribute put(String attrID, Object val) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Attribute remove(String attrID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public Object clone() {
+		return null;
+	}
+
+}
