1a430a5cac7ebb5129eac7df187df8455315c4cf
umeshma <umeshma@microsoft.com>
2011-03-31 15:24:08 -0700

    More coverage etc.

--- csharp/unittests/agent/ResolverTests.cs
+++ csharp/unittests/agent/ResolverTests.cs
@@ -13,12 +13,11 @@ Neither the name of The Direct Project (directproject.org) nor the names of its 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  
 */
+using System;
 using System.Collections.Generic;
 using System.Net.Mail;
 using System.Security.Cryptography.X509Certificates;
-
 using Health.Direct.Common.Certificates;
-
 using Xunit;
 using Xunit.Extensions;
 
@@ -71,6 +70,7 @@ namespace Health.Direct.Agent.Tests
     public class CertificateResolverTests
     {
         ICertificateResolver m_resolver;
+        MailAddress m_address = new System.Net.Mail.MailAddress("toby@redmond.hsgincubator.com");
         
         public CertificateResolverTests()
         {
@@ -156,6 +156,68 @@ namespace Health.Direct.Agent.Tests
         {
             X509Certificate2Collection certs = m_resolver.GetCertificates(new MailAddress(address));
             Assert.True(certs.IsNullOrEmpty());
+        }
+        
+        [Fact]
+        public void TestCollectionRetryNotFound()
+        {
+            CertificateResolverCollection resolvers = new CertificateResolverCollection();
+            Assert.True(resolvers.TryNextWhen == CertificateResolverCollection.TryNextCriteria.NotFound);
+
+            resolvers.Add(new NullResolver());
+            Assert.True(resolvers.GetCertificates(m_address).IsNullOrEmpty());
+
+            resolvers.Add(new ThrowingCertResolver());
+            Assert.Throws<InvalidOperationException>(() => resolvers.GetCertificates(m_address));
+            
+            resolvers.RemoveAt(0);
+            resolvers.Add(new NullResolver());
+            Assert.Throws<InvalidOperationException>(() => resolvers.GetCertificates(m_address));
+            
+            resolvers.Clear();
+            resolvers.Add(new NullResolver());
+            resolvers.Add(new NullResolver());
+            resolvers.Add(m_resolver);
+            Assert.True(!resolvers.GetCertificates(m_address).IsNullOrEmpty());            
+        }
+        
+        [Fact]        
+        public void TestCollectionRetryException()
+        {
+            CertificateResolverCollection resolvers = new CertificateResolverCollection();
+            resolvers.TryNextWhen = CertificateResolverCollection.TryNextCriteria.Exception;
+            
+            resolvers.Add(new NullResolver());
+            resolvers.Add(m_resolver);
+            Assert.True(resolvers.GetCertificates(m_address).IsNullOrEmpty());
+            
+            resolvers.TryNextWhen = CertificateResolverCollection.TryNextCriteria.NotFound;
+            Assert.True(!resolvers.GetCertificates(m_address).IsNullOrEmpty());
+            
+            resolvers.Clear();
+
+            resolvers.Add(new ThrowingCertResolver());
+            resolvers.Add(m_resolver);
+            Assert.Throws<InvalidOperationException>(() => resolvers.GetCertificates(m_address));
+            
+            resolvers.TryNextWhen = CertificateResolverCollection.TryNextCriteria.Exception;
+            Assert.True(!resolvers.GetCertificates(m_address).IsNullOrEmpty());
+        }
+        
+        [Fact]
+        public void TestCollectionRetryAlways()
+        {
+            CertificateResolverCollection resolvers = new CertificateResolverCollection();
+            resolvers.TryNextWhen = CertificateResolverCollection.TryNextCriteria.Always;
+            
+            resolvers.Add(new ThrowingCertResolver());
+            resolvers.Add(m_resolver);
+
+            Assert.DoesNotThrow(() => resolvers.GetCertificates(m_address));
+            Assert.True(!resolvers.GetCertificates(m_address).IsNullOrEmpty());
+            
+            resolvers.Insert(0, new NullResolver());
+            Assert.True(!resolvers.GetCertificates(m_address).IsNullOrEmpty());
         }
     }
 }
