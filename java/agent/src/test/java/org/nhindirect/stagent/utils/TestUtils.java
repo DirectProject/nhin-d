75f22057588985acaffb599bd1adc3d1fca0647f
Greg Meyer <gm2552@cerner.com>
2011-05-03 08:20:16 -0500

    Fixed issue with signature validation and added unit test. Updated release notes.

--- java/agent/src/test/java/org/nhindirect/stagent/utils/TestUtils.java
+++ java/agent/src/test/java/org/nhindirect/stagent/utils/TestUtils.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.security.Key;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.Security;
@@ -16,10 +17,13 @@ import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Enumeration;
 
 import org.apache.commons.io.FileUtils;
+import org.nhindirect.stagent.CryptoExtensions;
 import org.nhindirect.stagent.DefaultNHINDAgent;
 import org.nhindirect.stagent.NHINDAgentTest;
+import org.nhindirect.stagent.NHINDException;
 import org.nhindirect.stagent.cert.X509CertificateEx;
 import org.nhindirect.stagent.cert.impl.KeyStoreCertificateStore;
 import org.nhindirect.stagent.testmodules.AgentTestModule;
@@ -186,12 +190,67 @@ public class TestUtils
 	{
 		File fl = new File(certBasePath + certFileName);
 		
-		InputStream str = new ByteArrayInputStream(FileUtils.readFileToByteArray(fl));
+		byte[] data =  FileUtils.readFileToByteArray(fl);
 		
-		X509Certificate retVal = (X509Certificate)CertificateFactory.getInstance("X.509").generateCertificate(str);
+		X509Certificate retVal = certFromData(data);//(X509Certificate)CertificateFactory.getInstance("X.509").generateCertificate(str);
 		
-		str.close();
 		
 		return retVal;
 	}	
+	
+    private static X509Certificate certFromData(byte[] data)
+    {
+    	X509Certificate retVal = null;
+        try 
+        {
+            ByteArrayInputStream bais = new ByteArrayInputStream(data);
+            
+            // lets try this a as a PKCS12 data stream first
+            try
+            {
+            	KeyStore localKeyStore = KeyStore.getInstance("PKCS12", CryptoExtensions.getJCEProviderName());
+            	
+            	localKeyStore.load(bais, "".toCharArray());
+            	Enumeration<String> aliases = localKeyStore.aliases();
+
+
+        		// we are really expecting only one alias 
+        		if (aliases.hasMoreElements())        			
+        		{
+        			String alias = aliases.nextElement();
+        			X509Certificate cert = (X509Certificate)localKeyStore.getCertificate(alias);
+        			
+    				// check if there is private key
+    				Key key = localKeyStore.getKey(alias, "".toCharArray());
+    				if (key != null && key instanceof PrivateKey) 
+    				{
+    					retVal = X509CertificateEx.fromX509Certificate(cert, (PrivateKey)key);
+    				}
+    				else
+    					retVal = cert;
+    					
+        		}
+            }
+            catch (Exception e)
+            {
+            	// must not be a PKCS12 stream, go on to next step
+            }
+   
+            if (retVal == null)            	
+            {
+            	//try X509 certificate factory next       
+                bais.reset();
+                bais = new ByteArrayInputStream(data);
+
+                retVal = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(bais);            	
+            }
+            bais.close();
+        } 
+        catch (Exception e) 
+        {
+            throw new NHINDException("Data cannot be converted to a valid X.509 Certificate", e);
+        }
+        
+        return retVal;
+    }	
 }
