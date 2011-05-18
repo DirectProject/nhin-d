bd9fa129683df37631ea4abd0a36a6100e696443
Joe Shook <jshook@kryptiq.com>
2011-05-18 15:00:57 -0700

    Merging install_full directory and install back to just the install directory.

--- csharp/installer_full/createeventlogsource.ps1
+++
@@ -1,4 +0,0 @@
-if ([System.Diagnostics.EventLog]::SourceExists($args[0]) -eq $false) {
-    [System.Diagnostics.EventLog]::CreateEventSource($args[0], "Application")
-}
-write-eventlog -logname Application -source $args[0] -eventID 1 -entrytype Information -message $args[1]
