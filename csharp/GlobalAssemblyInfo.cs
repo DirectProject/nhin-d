8d049b5f53c876d3f366bcb090c8fa35d7d373b9
Joe Shook <jshook@kryptiq.com>
2011-07-28 16:34:07 -0700

    Advancing version number to 1.0.0.9...

--- csharp/GlobalAssemblyInfo.cs
+++ csharp/GlobalAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.8")]
-[assembly: AssemblyFileVersion("1.0.0.8")]
+[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyFileVersion("1.0.0.9")]
