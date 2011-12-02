4bf547de15c4d0d424f9c960a7ff927b8d3925ad
umeshma <umeshma@microsoft.com>
2011-12-02 15:37:49 -0800

    1. Nagle related bug 2. Removed incorrect Author attributions

--- csharp/resolverPlugin/ResolverExceptions.cs
+++ csharp/resolverPlugin/ResolverExceptions.cs
@@ -3,7 +3,6 @@
  All rights reserved.
 
  Authors:
-    Umesh Madan     umeshma@microsoft.com
     Joseph Shook    jshook@kryptiq.com
  
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
