f05ed2bfd8785711f0711223f19998dd28f95e86
joeshook <joeshook@localhost>
2011-12-13 10:48:33 -0800

    Advancing version number to 1.0.0.12...

--- csharp/GlobalAssemblyInfo.cs
+++ csharp/GlobalAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.11")]
-[assembly: AssemblyFileVersion("1.0.0.11")]
+[assembly: AssemblyVersion("1.0.0.12")]
+[assembly: AssemblyFileVersion("1.0.0.12")]
