97bd973493532f6eca19d325b554475e65a4ca86
gm2552 <gm2552@localhost>
2012-02-02 11:19:04 -0600

    Added performance test for CRLs and caching.

--- java/agent/src/test/java/org/nhindirect/stagent/cert/impl/CRLRevocationManager_performanceTest.java
+++ java/agent/src/test/java/org/nhindirect/stagent/cert/impl/CRLRevocationManager_performanceTest.java
@@ -6,6 +6,11 @@ import junit.framework.TestCase;
 
 import org.nhindirect.stagent.utils.TestUtils;
 
+/*
+ * This class may not fully run as part of a general build as it is intended to be a performance checker and requires
+ * heap memory to be increased (-Xmx1024m).  There are no assertions handled, so exceptions in the revocation manager
+ * should not affect general builds.
+ */
 public class CRLRevocationManager_performanceTest extends TestCase
 {
 	
@@ -13,7 +18,7 @@ public class CRLRevocationManager_performanceTest extends TestCase
 	{
 		
 	}
-/*
+
 	public void testGetCRL_multipleDispPoints() throws Exception
 	{
 		X509Certificate cert = TestUtils.loadCertificate("uhin.cer");
@@ -21,7 +26,39 @@ public class CRLRevocationManager_performanceTest extends TestCase
 		CRLRevocationManager mgr = CRLRevocationManager.getInstance();
 		mgr.flush();
 		
+		// time how long it takes to check revocation... first pass will have to download the CRL
+		
+		long startTime = System.currentTimeMillis();
 		mgr.isRevoked(cert);
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println("Revocation checking time for CRL download from URI: " + (endTime - startTime) + "ms");
+		
+		// time how long it takes to check revocation again, this should be done with the CRL in the cache
+		startTime = System.currentTimeMillis();
+		mgr.isRevoked(cert);
+		endTime = System.currentTimeMillis();
+		
+		System.out.println("Revocation checking time for CRL in memory cache: " + (endTime - startTime) + "ms");
+		
+		// delete memory cache and load from file
+		CRLRevocationManager.cache.clear();
+		
+		// let the system rest and catch up with the cache delete and file write
+		Thread.currentThread().sleep(2000);
+		
+		startTime = System.currentTimeMillis();
+		mgr.isRevoked(cert);
+		endTime = System.currentTimeMillis();
+		
+		System.out.println("Revocation checking time for CRL from file cache: " + (endTime - startTime) + "ms");
+		
+		// time how long it takes to check revocation again, this should be done with the CRL in the cache
+		startTime = System.currentTimeMillis();
+		mgr.isRevoked(cert);
+		endTime = System.currentTimeMillis();
+		
+		System.out.println("Revocation checking time for CRL in memory cache: " + (endTime - startTime) + "ms");
 	}
-*/
+
 }
