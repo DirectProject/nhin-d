c20ed75538eca6a46b2d637ae16aedda82dacda1
gm2552 <gm2552@localhost>
2013-07-22 14:53:29 -0500

    Adding tag for agent-2.0.2.

---
+++ java/tags/agent-2.0.2/src/test/java/org/nhindirect/stagent/cert/impl/AttributeAdapter.java
@@ -0,0 +1,90 @@
+package org.nhindirect.stagent.cert.impl;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.DirContext;
+
+@SuppressWarnings("serial")
+public class AttributeAdapter implements Attribute {
+
+	public boolean add(Object attrVal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void add(int ix, Object attrVal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean contains(Object attrVal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object get() throws NamingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object get(int ix) throws NamingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public NamingEnumeration<?> getAll() throws NamingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public DirContext getAttributeDefinition() throws NamingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public DirContext getAttributeSyntaxDefinition() throws NamingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isOrdered() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean remove(Object attrval) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object remove(int ix) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object set(int ix, Object attrVal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public Object clone() {
+		return null;
+	}
+
+}
