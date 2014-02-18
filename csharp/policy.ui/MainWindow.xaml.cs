7c494c7da738972b421886b18aadea603f74ce0c
Joe Shook <jshook@kryptiq.com>
2014-02-18 14:46:32 -0800

    Working on a UI policy validator and configurator

---
+++ csharp/policy.ui/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Health.Direct.Policy.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
