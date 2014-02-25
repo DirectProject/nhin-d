c063051f62a560a85cd76d5bd89cd00ae482dac1
joeshook <joeshook@gmail.com>
2014-02-25 12:00:53 -0800

    1. Still working on TeamCity build at TeamCity.CodeBetter.com 2. Starting to build policy datastore.

---
+++ csharp/config/store/CertPolicy.cs
@@ -0,0 +1,97 @@
+﻿/* 
+ Copyright (c) 2014, Direct Project
+ All rights reserved.
+
+ Authors:
+    Joe Shook     Joseph.Shook@Surescipts.com
+  
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of The Direct Project (directproject.org) nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+
+
+using System;
+using System.Data.Linq.Mapping;
+using System.Runtime.Serialization;
+using Health.Direct.Policy.Impl;
+
+
+namespace Health.Direct.Config.Store
+{
+    [Table(Name = "CertPolicy")]
+    [DataContract(Namespace = ConfigStore.Namespace)]
+    public class CertPolicy
+    {
+        public const int MaxNameLength = 400;
+
+        string m_Name;
+
+        [Column(Name = "CertPolicyId", IsPrimaryKey = true, IsDbGenerated = true, UpdateCheck = UpdateCheck.Never)]
+        [DataMember(IsRequired = true)]
+        public long ID 
+        { 
+            get; 
+            set; 
+        }
+
+        [Column(Name = "Name", CanBeNull = false, IsPrimaryKey = false )]
+        [DataMember(IsRequired = true)]
+        public string Name
+        {
+            get
+            {
+                return m_Name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ConfigStoreException(ConfigStoreError.InvalidCertPolicyName);
+                }
+
+                if (value.Length > MaxNameLength)
+                {
+                    throw new ConfigStoreException(ConfigStoreError.CertPolicyNameLength);
+                }
+
+                m_Name = value;
+            }
+        }
+
+
+        /// <summary>
+        /// For now thie will always be the <see cref="SimpleTextV1LexiconPolicyParser"/>.
+        /// 
+        /// </summary>
+        [Column(Name = "Lexicon", CanBeNull = false, IsPrimaryKey = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        [DataMember(IsRequired = true)]
+        public string Lexicon { 
+            get;
+            set;
+        }
+
+        [Column(Name = "Data", DbType = "varbinary(MAX)", CanBeNull = false, IsPrimaryKey = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        [DataMember(IsRequired = true)]
+        public byte[] PolicyData
+        {
+            get; 
+            set;
+        }
+
+
+        [Column(Name = "CreateDate", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        [DataMember(IsRequired = true)]
+        public DateTime CreateDate
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
---
+++ csharp/config/store/CertPolicyGroupDomainMap.cs
@@ -0,0 +1,82 @@
+﻿/* 
+ Copyright (c) 2014, Direct Project
+ All rights reserved.
+
+ Authors:
+    Joe Shook     Joseph.Shook@Surescipts.com
+  
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of The Direct Project (directproject.org) nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+
+using System;
+using System.Data.Linq.Mapping;
+using System.Runtime.Serialization;
+
+namespace Health.Direct.Config.Store
+{
+    [Table(Name = "CertPolicyGroupDomainMap")]
+    [DataContract(Namespace = ConfigStore.Namespace)]
+    public class CertPolicyGroupDomainMap
+    {
+        public const int MaxOwnerLength = 400;
+        string m_owner;
+
+        [Column(Name = "CertPolicyGroupDomainMapId", IsPrimaryKey = true, IsDbGenerated = true, UpdateCheck = UpdateCheck.Never)]
+        [DataMember(IsRequired = true)]
+        public long ID
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Relationshp to Domain.  Not enforced in SQL schema. 
+        /// </summary>
+        [Column(Name = "Owner", CanBeNull = false, IsPrimaryKey = false)]
+        [DataMember(IsRequired = true)]
+        public string Owner
+        {
+            get
+            {
+                return m_owner;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ConfigStoreException(ConfigStoreError.InvalidOwnerName);
+                }
+
+                if (value.Length > MaxOwnerLength)
+                {
+                    throw new ConfigStoreException(ConfigStoreError.OwnerLength);
+                }
+
+                m_owner = value;
+            }
+        }
+
+
+        [Column(Name = "CertPolicyGroupId", UpdateCheck = UpdateCheck.WhenChanged)]
+        [DataMember(IsRequired = true)]
+        public long CertPolicyGroupId
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "CreateDate", CanBeNull = false, UpdateCheck = UpdateCheck.WhenChanged)]
+        [DataMember(IsRequired = true)]
+        public DateTime CreateDate
+        {
+            get;
+            set;
+        }
+    }
+}
