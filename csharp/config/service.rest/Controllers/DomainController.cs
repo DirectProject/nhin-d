c7ecf4e6710c82db86e3760f8b3f53c24fd9cf54
Gerald Aden <gerald@gmatech.com>
2014-03-26 15:28:50 -0700

    Start implementation of rest service.

---
+++ csharp/config/service.rest/Controllers/DomainController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using Health.Direct.Config.Store;
+
+namespace Health.Direct.Config.Service.Controllers
+{
+    public class DomainController : ApiController
+    {
+        private readonly ConfigStore _configStore;
+
+        public DomainController()
+        {
+            _configStore = Service.Current.Store;
+        }
+
+        public DomainController(ConfigStore configStore)
+        {
+            _configStore = configStore;
+        }
+
+        public Domain GetDomain(long id)
+        {
+            return _configStore.Domains.Get(id);
+        }
+    }
+}
---
+++ csharp/config/service.rest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Health.Direct.Config.Service.WebApiApplication" Language="C#" %>
