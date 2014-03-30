14e1492489cd2fe293473a0b066db3d58b8aedf7
JoeShook <JoeShook@localhost>
2014-03-30 03:41:51 -0700

    removed circular ref to policy

--- csharp/common/Policies/IPolicyFilter.cs
+++ csharp/common/Policies/IPolicyFilter.cs
@@ -16,7 +16,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
-using Health.Direct.Policy;
+
 
 namespace Health.Direct.Common.Policies
 {
