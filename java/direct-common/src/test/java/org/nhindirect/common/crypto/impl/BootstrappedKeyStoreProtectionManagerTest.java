e34a0c163d6a59dc84a79b565477301a19b8be7f
gm2552 <gm2552@localhost>
2014-04-04 13:50:16 -0500

    Adding PKCS11 support and tests.  Also added PKCS11 secret key management tool.

--- java/direct-common/src/test/java/org/nhindirect/common/crypto/impl/BootstrappedKeyStoreProtectionManagerTest.java
+++ java/direct-common/src/test/java/org/nhindirect/common/crypto/impl/BootstrappedKeyStoreProtectionManagerTest.java
@@ -1,6 +1,12 @@
 package org.nhindirect.common.crypto.impl;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.security.Key;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.junit.Test;
 
@@ -29,4 +35,23 @@ public class BootstrappedKeyStoreProtectionManagerTest
 		assertTrue(Arrays.equals("1234".getBytes(), mgr.getKeyStoreProtectionKey().getEncoded()));
 		assertTrue(Arrays.equals("5678".getBytes(), mgr.getPrivateKeyProtectionKey().getEncoded()));
 	}	
+	
+	@Test
+	public void testGetAllKeys() throws Exception
+	{
+		BootstrappedKeyStoreProtectionManager mgr = new BootstrappedKeyStoreProtectionManager();
+		mgr.setKeyStoreProtectionKey("1234");
+		mgr.setPrivateKeyProtectionKey("5678");
+		
+		final Map<String, Key> keys = mgr.getAllKeys();
+		
+		assertEquals(2, keys.size());
+		
+		Iterator<Entry<String, Key>> entryIter = keys.entrySet().iterator();
+				
+		Key key = entryIter.next().getValue();		
+		assertTrue(Arrays.equals("5678".getBytes(), key.getEncoded()));
+		key = entryIter.next().getValue();	
+		assertTrue(Arrays.equals("1234".getBytes(), key.getEncoded()));
+	}
 }
